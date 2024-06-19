2b464ca66f25c45571c891b36bab7695f056c93c
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-06-19 12:45:01 +0200

    Update QuizDisplay.js

--- task/client/src/components/QuizDisplay.js
+++ task/client/src/components/QuizDisplay.js
@@ -2,19 +2,15 @@ import React, { useEffect, useState } from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+//QuizDisplay function
 export default function QuizDisplay() {
     //=========STATE VARIABLES====================
-    const [answers, setAnswers] = useState([])//=> store
+    const [answers, setAnswers] = useState([]);
     const [selectedQuiz, setSelectedQuiz] = useState(null)
     const [currentScore, setCurrentScore] = useState(0)
     const [currentQuestion, setCurrentQuestion] = useState(0)
     const [lastQuestion, setLastQuestion] = useState(false); // Indicates if the current question is the last one
     const [quizStarted, setQuizStarted] = useState(false)
-    const [play, setPlay] = useState(false)
-    //State variables to handle timer option
-    const [score, setScore] = useState(0);
-    const [timer, setTimer] = useState(10)
-    const [timerEnabled, setTimerEnabled] = useState(false);
     const [newQuizName, setNewQuizName] = useState('')
     const [questions, setQuestions] = useState([
         {
@@ -23,8 +19,13 @@ export default function QuizDisplay() {
             options: ['', '', '']
         }
     ]);
+    // const [score, setScore] = useState(0);
+     //State variables to handle timer option
 
-   
+    const [timer, setTimer] = useState(10)
+    const [timerEnabled, setTimerEnabled] = useState(false);
+    
+   //=============TIMER=====================
     //===========USE EFFECT HOOK TO DISPLAY TIMER OPTION============
     useEffect(() => {
         if
@@ -45,84 +46,123 @@ export default function QuizDisplay() {
         }
     }, [currentQuestion]);
 
-    //=========EVENT LISTENERS====================
+     useEffect(() => {
+        if (timeLeft === 0) {
+            return () => clearInterval(intervalRef)
+        }
+    },[timeLeft])
+
+        const handleStart= () => {
+        clearInterval(intervalRef.current); // Clear any existing interval
+        setTimeLeft(10); // Reset the countdown to 10 seconds
+
+        intervalRef.current = setInterval(() => {
+            setTimeLeft((prevTimeLeft) => {
+                if (prevTimeLeft > 0) {
+                    return prevTimeLeft - 1;
+                } else {
+                    clearInterval(intervalRef.current);
+                    return 0;
+                }
+            });
+        }, 1000); // Update every second
+    }
+ 
+
+    const handleStop = () => {
+        clearInterval(intervalRef.current)
+    }
+    
     // Handles the change in the timer option
-    const handleTimerChange = (event) => {
-        setTimerEnabled(event.target.checked); // Update timerEnabled based on the checkbox state
-    };
-  
+    // const handleTimerChange = (event) => {
+    //     setTimerEnabled(event.target.checked); // Update timerEnabled based on the checkbox state
+    // };
+
+    //=========EVENT LISTENERS====================
+
+    //Function to startQuiz
     const startQuiz = () => {
-        setPlay(true)
+        setQuizStarted(true)
     };
 
-    //=========================================
+      const handleSelect = (event) => {
+    try {
+      const quiz = JSON.parse(event.target.value)
+      setSelectedQuiz(quiz)
+      setCurrentQuestion(0)
+      setScore()
+    } catch (error) {
+      console.error('Error parsing quiz:', error);
+    }
+  }
+    //================JSX RENDERING=========================
   return (
     <div>
-        
-        {!play? (
+        {! quizStarted ? (
             <div>
-                <Row>
-                    <Col>
-                          <h2>SELECT QUIZ</h2>
-                    </Col>
-                </Row>
-                  <Row>
-                      <Col xs={12} md={8}>
-                      <label>
-                        <p>SELECT</p>
-                        <select onChange={(e) => startQuiz(quizList.find(q => q.category === e.target.value))}>
-                            {quizList.map((quiz, index) => (
-                                <option key={index} value={quiz.name}>{quiz.name}</option>
-                            ))}
-                            
-                        </select>
-                      </label>
-                      </Col>
-                      <Col xs={6} md={4}>
-                          <label>
-                              Add Timer:
-                              <input type="checkbox" onChange={(e) => setTimer(e.target.checked ? 60: null)} />
-                          </label>
-                      </Col>
-                  </Row>
-                  <Row>
-                      <Col xs={12} md={8}>
-                         
-                      </Col>
-                      <Col xs={6} md={4}>
-                          <button onClick={startQuiz}>PLAY</button>
-                      </Col>
-                  </Row>
-            </div>
+                <h3 className='h3'>{selectedQuiz.quizName}</h3>
+            <form>
+              <Row>
+                <Col xs={12} md={8}>
+                  <label>
+                    <p>SELECT</p>
+                    <select onChange={(e) => startQuiz(quizList.find(q => q.category === e.target.value))}>
+                      {quizList.map((quiz, index) => (
+                        <option key={index} value={quiz.name}>{quiz.name}</option>
+                      ))}
 
-           
-        ):(
+                    </select>
+                  </label>
+                </Col>
+                <Col xs={6} md={4}>
+                  <label>
+                    Add Timer:
+                    <input type="checkbox" onChange={(e) => setTimer(e.target.checked ? 60 : null)} />
+                  </label>
+                </Col>
+              </Row>
+              <h4>{/* question */}</h4>
+
+              <div>
+
+              </div>
+            </form>
+        </div>
+      ):(
             <div>
-                      <h3 className='h3'>{selectedQuiz.quizName}</h3>
-                <form>
-                
-                    <Row>
-                              <Col xs={6} md={4}>
-                              <p>TIMER:</p>
-                              </Col>
-                              <Col xs={12} md={8}>
-                              </Col>
-                    </Row>
-                          <h4>{/* question */}</h4>
-                    
-                    <div>
-                              {/* <Row>
-                                  <Col xs={12} md={8}>
-                                  <label><p> QUESTION:</p></label>
-                                  </Col>
-                                  <Col xs={6} md={4}>
-                                  <p>QUESTION x number out of 5</p>
-                                  </Col>
-                              </Row> */}
-                    </div>
-                </form>
-                </div>            
-        )}
-    </div>
-  )
+              <Row>
+                <Col>
+                  <h2 className='h2'>SELECT QUIZ</h2>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} md={8}>
+                  <label>
+                    <p>SELECT</p>
+                    <select onChange={(e) => startQuiz(quizList.find(q => q.category === e.target.value))}>
+                      {quizList.map((quiz, index) => (
+                        <option key={index} value={quiz.name}>{quiz.name}</option>
+                      ))}
+
+                    </select>
+                  </label>
+                </Col>
+                <Col xs={6} md={4}>
+                  <label>
+                    Add Timer:
+                    <input type="checkbox" onChange={(e) => setTimer(e.target.checked ? 60 : null)} />
+                  </label>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={12} md={8}>
+
+                </Col>
+                <Col xs={6} md={4}>
+                  <button onClick={startQuiz}>PLAY</button>
+                </Col>
+              </Row>
+           
+            </div>
+      )}  )
 }
