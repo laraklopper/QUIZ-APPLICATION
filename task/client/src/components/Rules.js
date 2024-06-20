3f0eabb4ac243b4028d81286a24d0f1953fe3ec4
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-06-20 15:48:05 +0200

    Update Rules.js

--- task/client/src/components/Rules.js
+++ task/client/src/components/Rules.js
@@ -1,7 +1,8 @@
-import React from 'react'
+// Import necessary modules and packages
+import React from 'react';// Import the React module to use React functionalities
 //Bootstrap
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row'; // Import the Row component from react-bootstrap
+import Col from 'react-bootstrap/Col'; // Import the Col component from react-bootstrap
 
 //Rules function component
 export default function Rules() {
