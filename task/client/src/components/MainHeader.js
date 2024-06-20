92c5bfa4f706e1aa258e39a39184e20608ddc811
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-06-20 15:47:27 +0200

    Update MainHeader.js

--- task/client/src/components/MainHeader.js
+++ task/client/src/components/MainHeader.js
@@ -1,40 +1,49 @@
+// Import necessary modules and packages
 import React from 'react';// Import the React module to use React functionalities
 //Bootstrap
-// Import Row and Col from react-bootstrap for layout
 import Row from 'react-bootstrap/Row'; // Import the Row component from react-bootstrap
 import Col from 'react-bootstrap/Col'; // Import the Col component from react-bootstrap
 // Import Link from react-router-dom for navigation
 import { Link } from 'react-router-dom';
 
 //MainHeader function component
-export default function MainHeader(
-  {mainHeading}//PROPS
-) {
+export default function MainHeader(//Export default MainHeader function component
+    {mainHeading} //Props
+    ) {
+
+    //========JSX RENDERING==========
+
   return (
-    <header className='header'>
-        <Row>
-            <Col>
+    // Header for the Login and Registration pages
+      <header className='header'>
+        {/* Heading Row */}
+          <Row className='headingRow'>
+              <Col className='heaingCol'>
+              {/* MainHeading of the page*/}
                   <h1 className='h1'>{mainHeading}</h1>
-            </Col>          
-        </Row>
-        <Row className='navRow'>
-            <Col className='navCol'>
-            <nav className='navigation'>
-                <ul className='navBar'>
-                    <li className='linkItem'>
-                        <Link className='refLink' to="/">
+              </Col>
+          </Row>
+          <Row className='navRow'>
+              <Col className='navCol'>
+              {/* NavigationBar */}
+                  <nav className='navigation'>
+                      <ul className='navbar'>
+                          <li className='linkItem'>
+                            {/* Link to Login Page */}
+                              <Link className='refLink' to='/'>
                                   <p className='linkText'>LOGIN</p>
-                        </Link>                       
-                    </li>
-                    <li className='linkItem'>
-                        <Link className='refLink' to="/reg">
-                            <p className='linkText'>REGISTRATION</p>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            </Col>
-        </Row>
-    </header>
+                              </Link>
+                          </li>
+                          <li className='linkItem'>
+                            {/* Link to Registration Page */}
+                              <Link className='refLink' to='/reg'>
+                                  <p className='linkText'>REGISTRATION</p>
+                              </Link>
+                          </li>
+                      </ul>
+                  </nav>
+              </Col>
+          </Row>
+      </header>
   )
 }
