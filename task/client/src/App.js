b321d983006014fd902e34ce705b690cfbd491af
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-06-21 12:50:12 +0200

    Update App.js

--- task/client/src/App.js
+++ task/client/src/App.js
@@ -13,16 +13,17 @@ import Registration from './Pages/Registration'; //Import RegistrationPage compo
 //App Function Components
 export default function App() {
   //=========STATE VARIABLES============
-  //user variables
-  const [users, setUsers] = useState([]);//State to store list of users
-  const [userData, setUserData] = useState({//State to store userData
+  //User variables
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userData, setUserData] = useState({
     username: '',
     email: '',
     dateOfBirth: '',
     admin: '',
     password: '',
   });
-  const [newUserData, setNewUserData] = useState({//State variable used to store Data about newUsers
+  const [newUserData, setNewUserData] = useState({
     newUsername: '',
     newEmail: '',
     newDateOfBirth: '',
@@ -32,42 +33,40 @@ export default function App() {
   // Quiz variables
   const [quizList, setQuizList] = useState([])
   //Event variables
-  const [error, setError] = useState(null);//State to handle errors during data fetching
-    //State variables to manage user Login
-  const [loggedIn, setLoggedIn] = useState(false);;// Boolean variable used to track if the user is logged in
+  const [error, setError] = useState(null);
+  //State variables to manage user Login
+  const [loggedIn, setLoggedIn] = useState(false);;
 
   //==========USE EFFECT HOOK TO FETCH ALL USERS==================
   //Fetch users when the component mounts or when loggedIn changes
   useEffect(() =>{
     //Function to fetch all users
-    const fetchUsers = async () => {//Define an async function to fetch all users
+    const fetchUsers = async () => {
       try {
-        const token = localStorage.getItem('token')//Retrieve token from localStorage
-        // Conditional rendering if token or login status is missing
+        const token = localStorage.getItem('token')
         if (!token || ! loggedIn) {
-          return; // Exit if no token is found or user is not logged in
+          return; 
         }
         //Send a GET request to the /users/findUsers endpoint
         const response = await fetch (`http://localhost:3001/users/findUsers`, {
-          method: 'GET',//HTTP request method
-          mode: 'cors',//Set the mode to cors allowing Cross-Origin-Requests
+          method: 'GET',
+          mode: 'cors',
           headers: {
-            'Content-Type': 'application/json',// Specify the Content-Type being sent in the request payload 
-            'Authorization': token,//Add the Authorization header containing the JWT token
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
           }
         });
         //Response handling
-        // Conditional rendering if the response indicates success (status code 200-299)
         if (!response.ok) {
-          throw new Error ('Failed to fetch user data');//Throw an error message if GET request is unsuccessful
+          throw new Error ('Failed to fetch user data');
         } 
 
-        const fetchedData = await response.json()//parse the response data as JSON
-        setUsers(fetchedData);//Log the fetchedData in the console for debugging purposes
+        const fetchedData = await response.json();
+        setUsers(fetchedData);
       } 
       catch (error) {
-        console.error('Error fetching user', error.message);//Log an error message in the console for debugging purposes
-        setError('Error fetching user');//Set an error message in the error state
+        console.error('Error fetching user', error.message);
+        setError('Error fetching user');
       }
     }
     fetchUsers()
@@ -75,14 +74,15 @@ export default function App() {
 
   //==========REQUESTS===============
   //--------GET-------------
-  //Function to fetch a single User
- /* const fetchUser = async (userId) => {
-    console.log('Fetch single user');
+  
+  //Function to fetch a currentUser
+ const fetchCurrentUser = async () => {
+    // console.log('Fetch current user');
     try {
       const token = localStorage.getItem('token');
       if (!token || !loggedIn) return
 
-      const response = await fetch (`http://localhost:3001/users/${userId}`, {
+      const response = await fetch ('http://localhost:3001/users/userId', {
         method: 'GET',
         mode: 'cors',
         headers: {
@@ -101,49 +101,47 @@ export default function App() {
           console.error('Error fetching user details:', error.message);
           setError('Error fetching user details:', error.message)
     }
-  }*/
+  }
 
   
   //Function to fetch Quizzes
   const fetchQuizzes = async () => {//Define an async function to fetch quizzes from the database
-    // console.log('Fetch quiz');
+    console.log('Fetch quiz');
     try {
-      const token = localStorage.getItem('token')//Retrieve the token from localStorage
+      const token = localStorage.getItem('token');
       //Send a GET request to the /users/fetchQuiz endpoint
       const response = await fetch('http://localhost:3001/users/fetchQuiz', {
-        method: 'GET',//HTTP request method
-        mode: 'cors',//Set the mode to cors, allowing cross-origin requests 
+        method: 'GET',
+        mode: 'cors',
         headers: {
-          'Content-Type': 'application/json',// Specify the Content-Type being sent in the request payload.
+          'Content-Type': 'application/json',
           'Authorization': `Bearer ${token}`,
         }
       })
       //Response handling
-// Conditional rendering if the response indicates success (status code 200-299)
-
       if (response.ok) {
         const quizData = await response.json();// Parse JSON response
         setQuizList(quizData);
-        // console.log(quizData);//Log a message in the console for debugging purposes
+        console.log(quizData);
       } else {
-        throw new Error('Failed to fetch quizzes')//Throw an error message stating theat the request is unsuccessful
+        throw new Error('Failed to fetch quizzes');
       }
     } catch (error) {
-      console.error('Error fetching quizzes:', error);//Log an error message in the console for debuggging purposes
-      setError('Error fetching quizzes:', error)//Set the error state with an error message
+      console.error('Error fetching quizzes:', error);
+      setError('Error fetching quizzes:', error);
     }
   }
   //---------POST---------------
   //Function to submitLogin
   const submitLogin = async (e) => {//Define an async function to submit user login
-    e.preventDefault();// Prevent the default form submission behavior
+    e.preventDefault();
     try {
-            // Send a POST request to the server for user login
+      // Send a POST request to the server for user login
       const response = await fetch('http://localhost:3001/users/login',{
-        method: 'POST',//Http request method
-        mode: 'cors',//Set the mode to cors, allowing cross-origin requests 
+        method: 'POST',
+        mode: 'cors',
         headers: {
-          'Content-Type': 'application/json',// Specify the Content-Type being sent in the request payload.
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({// Convert user login data to JSON string
           username: userData.username,
@@ -151,12 +149,12 @@ export default function App() {
           password: userData.password
         })
       })
-// Conditional rendering if the response indicates success (status code 200-299)
+
+      //Response handling
       if (response.ok) {
         const data = await response.json();//Parse the response data as JSON
-        localStorage.setItem('username', userData.username)// Store the user's username in the localStorage under the key 'username'
-        localStorage.setItem('loggedIn', true);/* Store the login status of the user in the localStorage under the key 'loggedIn'
-        Setting it to true indicates that the user is logged in*/
+        localStorage.setItem('username', userData.username)
+        localStorage.setItem('loggedIn', true);
         localStorage.setItem('token', data.token)/* Store the authentication token received 
         from the server in the localStorage under the key 'token'*/
 
@@ -169,26 +167,26 @@ export default function App() {
           password: ' ',
         })
 
-        console.log('User logged In');//Log a message in the console for debugging purposes
-        console.log(data);//Log a message in the console for debugging purposes
+        console.log('User logged In');
+        console.log(data);
       } else {
         throw new Error('Username or password are incorrect')
       }
     } catch (error) {
-      setError(`Login Failed ${error.message}`);//Set the error state with an error message
-      console.log(`Login Failed ${error.message}`); //Log an error message in the console for debugging purposes
-      setLoggedIn(false);//Update the logged in State to false
+      setError(`Login Failed ${error.message}`);
+      console.log(`Login Failed ${error.message}`);
+      setLoggedIn(false);
   }
   }
     //Function to add a new user
   const addUser = async (e) => {//Define an async function to add a new user
-    console.log('register new user');//Log a message in the console for debugging purposes
-    e.preventDefault();//Prevent default form submission behaviour
+    console.log('register new user');
+    e.preventDefault();
     try {
       //Send a POST request to the register endpoint
       const response = await fetch ('http://localhost:3001/users/register', {
-        method: 'POST',//HTTP request method
-        mode: 'cors',//Set the mode to cors allowing cross-origin requests
+        method: 'POST',
+        mode: 'cors',
         headers: {
           'Content-Type': 'application/json',
         },
@@ -200,7 +198,8 @@ export default function App() {
           password: newUserData.newPassword,
         })
       })
-// Condtitional rendering to check if if the response indicates success (status code 200-299)
+
+      //Response handling
       if (response.ok) {
         console.log('user successfully registerd.');
         setNewUserData(
@@ -211,13 +210,14 @@ export default function App() {
             newAdmin: false,
             newPassword: ''
           });
-      } else {
-        throw new Error ('Error adding user')//Throw an error message indicating the request was unsuccessful
+      } 
+      else {
+        throw new Error ('Error adding user');
       }
 
     } catch (error) {
-      setError(`Error adding new user: ${error.message}`); //Set the error state with an error message
-      console.error(`Error adding new user: ${error.message}`);  //Log an error message in the console for debugging purposes
+      setError(`Error adding new user: ${error.message}`); 
+      console.error(`Error adding new user: ${error.message}`);  
     }
   }
   //========EVENT LISTENERS==========
@@ -225,10 +225,9 @@ export default function App() {
    /*Function to set the loggedOut status to false
   stating that the user is logged in*/
    const appLogin = () => {
-     let token = localStorage.getItem('token'); // Retrieve token from localStorage
-     //Condtional rendering to check if the token exists
+     let token = localStorage.getItem('token');
      if (token) {
-       setLoggedOut(false);// Set loggedOut state to false, indicating the user is logged in
+       setLoggedOut(false);
      }
    }
 
@@ -240,7 +239,6 @@ export default function App() {
     setLoggedIn(false)
     setError('');
     setUserData({ username: '', password: '' });
-    ;
   }
 
   //=========JSX RENDERING=============
@@ -250,31 +248,19 @@ export default function App() {
         <Container className='appContainer'>
           <Routes>
             {loggedIn ? (
-              <Route path='/home' element={<Page1 logout={logout}/>} />
+              <Route path='/home' element={<Page1 logout={logout} />} />
+              <Route path='/page2' element={ <Page2 logout={logout} fetchQuizzes={fetchQuizzes}/>}/>
+              <Route path='/page3' element={<Page3  fetchQuizzes={fetchQuizzes} quizList={quizList} logout={logout}/>}/>
+               <Route path='/page4' element={ <Page4 setError={setError} logout={logout} currentUser={currentUser} setUsers={setUsers} setLoggedIn={setLoggedIn} />}/>
             ) : (
               <>
-                <Route path='/' element={
-                    <Login
-                      submitLogin={submitLogin}
-                      userData={userData}
-                      setUserData={setUserData}
-                    />
-                  }
-                />
-                <Route path='/reg' element={
-                    <Registration
-                      addUser={addUser}
-                      newUserData={newUserData}
-                      setNewUserData={setNewUserData}
-                    />
-                  }
-                />
+                <Route path='/' element={ <Login submitLogin={submitLogin} userData={userData}setUserData={setUserData}/>} />
+                <Route path='/reg' element={ <Registration addUser={addUser} newUserData={newUserData} setNewUserData={setNewUserData}/>}/>
               </>
             )}
           </Routes>
         </Container>
       </BrowserRouter>
-    </>
- 
+    </> 
   )
   }
