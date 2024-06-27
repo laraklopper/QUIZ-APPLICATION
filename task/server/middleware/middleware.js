36586c6f08132ffdc18e62e0b98b6f1ac33d2993
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-06-27 15:30:26 +0200

    Update middleware.js

--- task/server/middleware/middleware.js
+++ task/server/middleware/middleware.js
@@ -24,6 +24,34 @@ const authenticateToken = (req, res, next) => {
     });
 };
 
+//Middleware function to check and verify a JWT token from the 'token' header
+const checkJwtToken = (req, res, next) => {
+        // Retrieve the token from the Authorization header, if it exists, and remove the 'Bearer ' prefix
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    //Conditional rendering to check if the token is present
+    if (!token) {
+        // console.log('Unauthorized: token missing');
+        // If no token is found, respond with a 401 (Unauthorized) status code and an error message
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+    try {
+        const decoded = jwt.verify(
+            token,
+            'Secret-Key',//SecretKey
+            /*process.env.JWT_SECRET*/
+        );
+                // Attach the decoded user information to the request object for further use in the request lifecycle
+        req.user = decoded;
+        next();//Call the next middleware function
+    } catch (ex) {
+        //ex is the variable that holds the caught exception or error
+        // console.error('No token attatched to the request');
+        // If token verification fails, respond with a 400 status code and an error message
+        res.status(400).json({ message: 'Invalid token.' });
+    }
+} //If no exception is thrown in the try block, the catch block is skipped
+/*
 //Middleware for password validation
 const passwordValidation = (req, res, next) => {
     const { password } = req.body; // Extract the password from the request body
@@ -31,14 +59,14 @@ const passwordValidation = (req, res, next) => {
     // Regular expression to validate password requirements
     const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-    /*
-    This regex ensures that the password meets the following criteria:
-    - At least one uppercase letter: (?=.*[A-Z])
-    - At least one lowercase letter: (?=.*[a-z])
-    - At least one digit: (?=.*\d)
-    - At least one special character from the set @$!%*?&: (?=.*[@$!%*?&])
-    - Minimum length of 8 characters: [A-Za-z\d@$!%*?&]{8,}
-    */
+ 
+    // This regex ensures that the password meets the following criteria:
+    // - At least one uppercase letter: (?=.*[A-Z])
+    // - At least one lowercase letter: (?=.*[a-z])
+    // - At least one digit: (?=.*\d)
+    // - At least one special character from the set @$!%*?&: (?=.*[@$!%*?&])
+    // - Minimum length of 8 characters: [A-Za-z\d@$!%*?&]{8,}
+    
 
     if (!passwordRegex.test(password)) {
         return res.status(400).json({// Respond with 400 if the password does not meet the requirements
@@ -50,34 +78,7 @@ const passwordValidation = (req, res, next) => {
 
     next(); // Call the next middleware function
 };
-
-//Middleware function to check and verify a JWT token from the 'token' header
-const checkJwtToken = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (!token) {
-        console.log('Unauthorized: token missing');
-        return res.status(401).json({ message: 'Unauthorized: Token missing'})
-    }
-
-    try {
-        const decoded = jwt.verify(
-            token,
-            'secretKey'
-        )
-
-        req.user = decoded
-
-        console.log('Token verified');
-        next()
-
-    } 
-    catch (error) {
-        console.error('No token attatched to the request');
-        return res.status(401).json({message: 'Forbidden: Invalid Token'})
-    }
-};
+*/
 
 //Middleware function to check that admin user is 18 years or older
 const checkAge = (req, res, next) => {
@@ -98,21 +99,21 @@ const checkAge = (req, res, next) => {
 }
 
 // Middleware to check if the user is above 18 if admin is selected
-const checkAdminAge = (req, res, next) => {
-    if (req.body.admin) {
-        const today = new Date();
-        const birthDate = new Date(req.body.dateOfBirth);
-        const age = today.getFullYear() - birthDate.getFullYear();
-        const m = today.getMonth() - birthDate.getMonth();
-        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
-        }
-        if (age < 18) {
-            return res.status(400).json({ error: 'Admin users must be above 18 years old' });
-        }
-    }
-    next();
-};
+// const checkAdminAge = (req, res, next) => {
+//     if (req.body.admin) {
+//         const today = new Date();
+//         const birthDate = new Date(req.body.dateOfBirth);
+//         const age = today.getFullYear() - birthDate.getFullYear();
+//         const m = today.getMonth() - birthDate.getMonth();
+//         if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+//             age--;
+//         }
+//         if (age < 18) {
+//             return res.status(400).json({ error: 'Admin users must be above 18 years old' });
+//         }
+//     }
+//     next();
+// };
 
 module.exports = {authenticateToken, checkJwtToken, checkAge, passwordValidation}
 
