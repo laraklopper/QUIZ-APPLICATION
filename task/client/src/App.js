9fe0c4ead8cf7d55605fbbaced6c606e1e221a21
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-06-27 16:16:31 +0200

    Add files via upload

---
+++ task/client/src/App.js
@@ -0,0 +1,285 @@
+// Import necessary modules and packages
+import React, { useEffect, useState } from 'react';// Import the React module to use React functionalities
+import './App.css';//Import CSS stylesheet
+// React Router components
+import { BrowserRouter, Route, Routes } from 'react-router-dom';// React Router components for routing
+//Bootstrap
+import Container from 'react-bootstrap/Container';
+//Pages
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+import Page3 from './pages/Page3';
+import Page4 from './pages/Page4';
+
+//App function component
+export default function App() {
+  //=======STATE VARIABLES===============
+  //User variables
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userData, setUserData] = useState({
+    username: '',
+    email: '',
+    dateOfBirth: '',
+    admin: '',
+    password: '',
+  });
+  const [newUserData, setNewUserData] = useState({
+    newUsername: '',
+    newEmail: '',
+    newDateOfBirth: '',
+    newAdmin: false,
+    newPassword: ''
+  });
+  //Quiz variables
+  const [quizList, setQuizList] = useState([]);
+  //Event variables
+  const [error, setError] = useState(null);
+  //State variables to manage user Login
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  //============USE EFFECT HOOK TO FETCH USERS======================
+  //Fetch users when the component mounts or when loggedIn changes
+  useEffect(() => {
+    //Function to fetch users
+    const fetchUsers = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token || !loggedIn) return;
+
+        const response = await fetch('http://localhost:3001/users/findUsers', {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch users');
+        }
+
+        const fetchedUsers = await response.json();
+        setUsers(fetchedUsers);
+      } catch (error) {
+        console.error('Error fetching users', error.message);
+        setError('Error fetching users');
+      }
+    };
+
+    //Function to fetch the current user details
+    const fetchCurrentUser = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) return;
+
+        const response = await fetch('http://localhost:3001/users/userId', {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch current user');
+        }
+
+        const fetchedCurrentUser = await response.json();
+        setCurrentUser(fetchedCurrentUser);
+      } catch (error) {
+        console.error('Error fetching current user', error.message);
+        setError('Error fetching current user');
+      }
+    };
+
+    if (loggedIn) {
+      fetchUsers();
+      fetchCurrentUser();
+    }
+  }, [loggedIn]);
+
+  //==============REQUESTS========================
+  //-----------GET-------------------------
+// Function to fetch quizzes
+  const fetchQuizzes = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      const response = await fetch('http://localhost:3001/quiz/fetchQuizzes', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch quizzes');
+      }
+
+      const quizData = await response.json();
+      setQuizList(quizData);
+    } catch (error) {
+      console.error('Error fetching quizzes:', error);
+      setError('Error fetching quizzes');
+    }
+  };
+
+  //---------POST---------------------------
+  //Function to submit Login
+  const submitLogin = async (e) => {
+    try {
+      const response = await fetch('http://localhost:3001/users/login', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: userData.username,
+          password: userData.password
+        })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('username', userData.username);
+        localStorage.setItem('loggedIn', true);
+        localStorage.setItem('token', data.token);
+        setLoggedIn(true);
+        setError(null);
+        setUserData({
+          username: '',
+          password: '',
+        });
+      } else {
+        throw new Error('Username or password are incorrect');
+      }
+    } catch (error) {
+      setError(`Login Failed: ${error.message}`);
+      console.log(`Login Failed: ${error.message}`);
+      setLoggedIn(false);
+    }
+  };
+
+  //Function to register a new user
+  const addUser = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/users/register', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: newUserData.newUsername,
+          email: newUserData.newEmail,
+          dateOfBirth: newUserData.newDateOfBirth,
+          admin: newUserData.newAdmin,
+          password: newUserData.newPassword
+        })
+      });
+
+      if (response.ok) {
+        setNewUserData({
+          newUsername: '',
+          newEmail: '',
+          newDateOfBirth: '',
+          newAdmin: false,
+          newPassword: ''
+        });
+        alert('New User successfully registered')
+      } else {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Error adding user');
+      }
+    } catch (error) {
+      setError(`Error adding new user: ${error.message}`);
+      console.error(`Error adding new user: ${error.message}`);
+    }
+  };
+//===========EVENT LISTENERS============================
+
+  //Function to trigger logoutbtn
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    localStorage.removeItem('loggedIn');
+    setLoggedIn(false);
+    setError('');
+    setUserData({ username: '', password: '' });
+  };
+
+  //===========JSX RENDERING=============================
+
+  return (
+    <>
+      <BrowserRouter>
+        <Container>
+          <Routes>
+            {loggedIn ? (
+              <>
+                <Route path='/' element={
+                  <Page1
+                    logout={logout}
+                    error={error}
+                    currentUser={currentUser}
+                  />}
+                />
+                <Route path='/page2' element={
+                  <Page2
+                    logout={logout}
+                    fetchQuizzes={fetchQuizzes}
+                    quizList={quizList}
+                  />}
+                />
+                <Route path='/page3' element={
+                  <Page3
+                    // fetchQuizzes={fetchQuizzes}
+                    quizList={quizList}
+                    logout={logout}
+                    setError={setError}
+                    setQuizList={setQuizList}
+                    fetchQuizzes={fetchQuizzes}
+                  />}
+                />
+                <Route path='/page4' element={
+                  <Page4
+                    setError={setError}
+                    logout={logout}
+                    currentUser={currentUser}
+                    setUsers={setUsers}
+                    setLoggedIn={setLoggedIn}
+                    users={users}
+                  />}
+                />
+              </>
+            ) : (
+              <>
+                <Route exact path='/' element={
+                  <Login
+                    submitLogin={submitLogin}
+                    userData={userData}
+                    setUserData={setUserData}
+                  />}
+                />
+                <Route path='/reg' element={
+                  <Registration
+                    addUser={addUser}
+                    newUserData={newUserData}
+                    setNewUserData={setNewUserData}
+                  />}
+                />
+              </>
+            )}
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </>
+  );
+}
