8088bb38ef259f791ac68097533e5c284a3d6d6e
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-06-29 10:54:22 +0200

    Update EditQuiz.js

--- task/client/src/components/EditQuiz.js
+++ task/client/src/components/EditQuiz.js
@@ -106,7 +106,7 @@ export default function EditQuiz(
         <Row>
           <Col xs={12} md={8}></Col>
           <Col xs={6} md={4}>
-            <Button type="submit" variant="primary">Save Changes</Button>
+            <Button type="submit" variant="primary">EDIT</Button>
           </Col>
         </Row>
       </form>
