f53a5c7de6924ac6f07eedd2a647994be8380645
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-06-29 15:12:53 +0200

    Update Page3.js

--- task/client/src/pages/Page3.js
+++ task/client/src/pages/Page3.js
@@ -19,25 +19,28 @@ export default function Page3(//Export default Page3 function component
   setError,
   fetchQuizzes
 }) {
-
-  // ========STATE VARIABLES===============
-  const [quizName, setQuizName] = useState('');//Stores the name of the new quiz
+  //=========STATE VARIABLES
+  // State variables for managing quiz creation and editing
+const [quizName, setQuizName] = useState('');//State used to store the name of the new quiz
   const [questions, setQuestions] = useState([//State used to store an array of question objects, each containing questionText, correctAnswer, and options.
     { questionText: '', correctAnswer: '', options: ['', '', ''] }
   ]);
-  //Update quiz variables
-  const [updateQuiz, setUpdateQuiz] = useState(null); //State used to store the ID of the quiz being updated 
-  const [newQuizName, setNewQuizName] = useState('');
-  const [newQuestion, setNewquestion] = useState([
+  const [update, setUpdate] = useState(false);// State used to flag the toggle quiz update mode
+  const [quizToUpdate, setQuizToUpdate] = useState(null);//State used to store the ID of the quiz being updated 
+  const [newQuizName, setNewQuizName] = useState('');//State used to store the new updated name of a quiz being edited
+  const [newQuestion, setNewQuestion] = useState([// Array of new questions when editing a quiz
     { newQuestionText: '', newCorrectAnswer: '', newOptions: ['', '', ''] }
   ]);
   const [formError, setFormError] = useState('');//State used to store any error messages related to the form.
-
+  
 //========================================================
-  // useEffect to fetch quizzes when component mounts
+  /* useEffect to fetch quizzes when the component mounts 
+  or when fetchQuizzes function changes*/
   useEffect(() => {
     fetchQuizzes()//call the fetchQuizzes function
-  },[fetchQuizzes])
+  },[fetchQuizzes]) // Dependency array ensures fetchQuizzes is called only when it changes
+/*[fetchQuizzes] ensures that fetchQuizzes is re-invoked whenever its reference changes, 
+which is typical when it's passed as a prop and could change due to parent component re-renders or updates.*/
 
   // ==============REQUESTS=======================
   // ----------POST-------------------
@@ -46,30 +49,33 @@ export default function Page3(//Export default Page3 function component
     event.preventDefault();//Prevent default form submission
     //Conditional rendering to check that the quiz contains exactly 5 questions
     if (questions.length !== 5) {
-      setFormError('You must add exactly 5 questions.');//If the quiz consists of more than 5 questions set a form error
+      setFormError('You must add exactly 5 questions.'); // Set form error if validation fails
       return;// Exit the function
     }
       // If the validation passes, proceed with adding the quiz
     try {
-      const token = localStorage.getItem('token');//Retrieve the token from local storage
+      const token = localStorage.getItem('token');//Retrieve the authentication token from local storage
+      /*Retrieves the authentication token from local storage for use in the request header*/
       //Send a POST request to the server
       const response = await fetch('http://localhost:3001/quiz/addQuiz', {
         method: 'POST',//HTTP request method
         mode: 'cors',//Set the mode to cors, allowing cross-origin requests
         headers: {
           'Content-Type': 'application/json',// Specify the Content-Type being sent in the request payload. 
-          'Authorization': `Bearer ${token}`// Pass the JWT token in Authorization header
+          'Authorization': `Bearer ${token}`// Set authorization header with the JWT token
         },
-        body: JSON.stringify({ quizName, questions })// Convert the quizName and questions state to a JSON string for the request body
+        body: JSON.stringify(// Convert the quizName and questions state to a JSON string for the request body
+          { quizName, questions }
+        )
       });
 
       //Response handling
-      // Conditional rendering to check if the response indicates success (status code 200-299)
+      // Conditional rendering to check if the response is not okay (status code not in 200-299 range)
       if (!response.ok) {
         throw new Error('There was an error creating the quiz');//Throw an error message if the POST request is unsuccessful
       }
 
-      const quiz = await response.json();//Parse the data as JSON
+      const quiz = await response.json();// Parse the JSON response to get the created quiz
       //Reset the Form fields
       setQuizList([...quizList, quiz]);// Update the quiz list state with the new quiz
       setQuizName('');//Reset the quizName
@@ -102,7 +108,10 @@ export default function Page3(//Export default Page3 function component
           'Authorization': `Bearer ${token}`, // Include the authorization token in the request header
         },
         body: JSON.stringify(// Convert newQuizName and newQuestion to a JSON string for the request body
-          { quizName: newQuizName, questions: newQuestion }
+          {// Request body containing the updated quiz name and questions
+            quizName: newQuizName, //The updated name for the quiz being edited.
+            questions: newQuestion // The updated list of questions for the quiz
+          }// Convert updated quiz data to JSON format
         )
       });
       //Response handling
@@ -112,14 +121,15 @@ export default function Page3(//Export default Page3 function component
                /* Update the quizList state by mapping through the current list (quizList.map) and replacing 
        the quiz with matching ID (q._id === updatedQuiz._id) with the updated quiz (updatedQuiz).*/
         setQuizList(quizList.map(q => (q._id === updatedQuiz._id ? updatedQuiz : q)));
-// Reset form fields and state
-        setQuizName('');
-        setUpdateQuiz(null);// Exit the update mode
+        // Reset form fields and state
+        setQuizName('');//Clear the newQuizName input field
+        setUpdateQuiz(null);// Clear the quizToUpdate state to exit edit mode
       } 
       else {
         throw new Error('Error editing quiz');//Throw an error message if the PUT request is unsuccessful
       }
     } catch (error) {
+          // Handle any errors that occur during the request
       console.error(`Error editing the quiz: ${error}`);//Log an error message in the console for debugging purposes
       setError(`Error editing the quiz: ${error}`)//Set the error state with an error message
     }
@@ -129,8 +139,8 @@ export default function Page3(//Export default Page3 function component
 //Function to delete a quiz
   const deleteQuiz = async (quizId) => {//Define an async function to delete a quiz
     try {
-      const token = localStorage.getItem('token');//Retrieve the token from localStorage
-      //Send a delete request to server
+      const token = localStorage.getItem('token');//Retrieve the authentucation token from localStorage
+      //Send a DELETE request to server
       const response = await fetch(`http://localhost:3001/quiz/deleteQuiz/${quizId}`, {
         method: 'DELETE',//HTTP request method
         mode: 'cors',// Enable Cross-Origin Resource Sharing (CORS)
@@ -139,9 +149,15 @@ export default function Page3(//Export default Page3 function component
           'Authorization': `Bearer ${token}`,// Include the authorization token in the request header
         }
       });
+
+      //Response handling
+      // Conditional rendering to check if the response indicates success (status code 200-299)
       if (response.ok) {
               // If successful, update the quizList state by filtering out the deleted quiz
         setQuizList(quizList.filter(q => q._id !== quizId));
+        /*The filter method iterates through each quiz in quizList. For each quiz q, it checks if q._id is not equal to quizId. 
+        If q._id is not equal to quizId, the quiz is included in the new array.*/
+        //setQuizList(...): Updates the state with the new array that excludes the deleted quiz.
       } else {
         throw new Error('Error deleting quiz');//Throw an error message if the DELETE request is unsuccessful
       }
@@ -164,9 +180,9 @@ const handleChange = (index, event) => {
   } else {
     // If the target name is an option (e.g., options.0, options.1, options.2)
     const optionIndex = Number(event.target.name.split('.')[1]);    // Extract the option index from the event target name
-
-    // Update the option value in the options array of the question at the specified index
-    values[index].options[optionIndex] = event.target.value;
+/*The string.split() method takes a pattern and divides the string into an ordered list of substrings 
+by searching for the pattern, puts these substrings into an array, and returns the array.*/
+    values[index].options[optionIndex] = event.target.value;// Update the option value in the options array of the question at the specified index
   }
 
   // Update the state (questions) with the updated values array
@@ -220,12 +236,13 @@ const handleAddQuestion = () => {
     setFormError('');    // Clear any existing form errors
 
   }
-
-  //Function to toggle editForm
-  const toggleQuizUpdate = (quizId) => {
-      // Toggle the update state based on whether the clicked quizId matches the current updateQuiz state
-    setUpdateQuiz(quizId === updateQuiz ? null : quizId);
-  };
+// Function to toggle the edit mode of a quiz
+const toggleQuizUpdate = (quizId) => {
+  // Toggle the update state between true and false
+  setUpdate(!update);
+  // Set the quiz ID of the quiz to be updated
+  setQuizToUpdate(quizId);
+};
 
 
   //==============JSX RENDERING=================
@@ -248,9 +265,11 @@ const handleAddQuestion = () => {
             <Row  className='quizListRow'>
               <Col className='quizRow'><p>Quiz Name: {quiz.quizName}</p></Col>
               {/* Toggle button to update QUIZ */}
-              <Col> <Button variant='primary' type='button' onClick={() => toggleQuizUpdate(quiz._id)}>
-                {updateQuiz === quizName._id ? 'EXIT': 'EDIT'}
-                </Button></Col>
+              <Col> 
+          <Button variant='primary' type='button' onClick={() => toggleQuizUpdate(quiz._id)}>
+                    {update && quizToUpdate === quiz._id ? 'EXIT' : 'EDIT'}
+                  </Button>
+          </Col>
             {updateQuiz === quizName._id && (
                 <EditQuiz 
                 quiz={quiz} 
