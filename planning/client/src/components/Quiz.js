7b0d20a79097b33969171b19c2586f02a9a713ce
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-07-03 12:15:29 +0200

    Update Quiz.js

--- planning/client/src/components/Quiz.js
+++ planning/client/src/components/Quiz.js
@@ -1,65 +1,72 @@
-//Quiz.js
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+//Bootstrap
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-//Quiz function component
-export default function Quiz  ({ 
-  questions, 
-  handleNextQuestion, 
-  currentQuestion, 
-  handleAnswerClick, 
-  timer, 
-  lastQuestion 
-}
-) {
-
+export default function Quiz({timer, handleAnswerClick, currentQuestion, handleNextQuestion, lastQuestion, questions}) {
   const optionIds = ['A', 'B', 'C', 'D'];
   const [selectedOption, setSelectedOption] = useState(null);
-  
+
   const handleOptionClick = (option) => {
     setSelectedOption(option);
     handleAnswerClick(option);
   };
-
-  //==========JSX RENDERING==============
-  
   return (
     <div>
-      <div>
-        <p>
-          Time remaining: {timer}
-        </p>
-        <h4>
-          {questions[currentQuestion].id}
-          {questions[currentQuestion].question}
-        </h4>
-        <div>
-          {questions[currentQuestion].options.map((option, index) => (
-            <Button key={index} onClick={() => handleOptionClick(option)}>
-              {optionIds[index]}) {option}
-            </Button>
-          ))}
+      <Row>
+        <Col xs={6} md={4}>
+          <p>Time remaining: {timer}</p>
+         
+          </Col>
+        <Col xs={6} md={4}>
+          <h4>
+            {questions[currentQuestion].id}
+            {questions[currentQuestion].question}
+          </h4>
+          
+
+        </Col>
+        <Col xs={6} md={4}>
+        </Col>
+        </Row>
+        <Row>
+        <Col xs={6} md={4}>
+        </Col>
+        <Col xs={6} md={4}>
+        <div id='options'>
+          <Row id='questionsRow'>
+            <Col>
+            {questions[currentQuestion].options.map((option, index) => (
+              <Button key={index} onClick={() => handleOptionClick(option)}>
+                {optionIds[index]} {option}
+              </Button>
+            ))}
+            </Col>
+          </Row>
+
         </div>
-        <br/>
-        <div>
-          <p>
-            Question {currentQuestion + 1} of {questions.length}
-          </p>
-          <div>
-            {lastQuestion ? (
-              <button onClick={handleNextQuestion}>
-                Submit
-              </button>
-            ) : (
-              <button onClick={handleNextQuestion}>
-                Next Question
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
+         
+        </Col>
+        <Col xs={6} md={4}>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={6} md={4}>
+        </Col>
+        <Col xs={6} md={4}>
+          {lastQuestion ? (
+            <button onClick={handleNextQuestion}>
+              Submit
+            </button>
+          ) : (
+            <button onClick={handleNextQuestion}>
+              Next Question
+            </button>
+          )}      </Col>
+        <Col xs={6} md={4}>
+        </Col>
+      </Row>
     </div>
-  );
-};
+  )
+}
