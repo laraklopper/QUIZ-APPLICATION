61bc63e1488bf04f2ab82b04d74a3d490656f74a
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-07-03 08:15:29 +0200

    Update Quiz.js

--- task/client/src/components/Quiz.js
+++ task/client/src/components/Quiz.js
@@ -1,12 +1,14 @@
 // Import necessary modules and packages
 import React from 'react'
+// Bootstrap
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+//Component
 import Timer from './Timer';
 
 //Quiz Function Component
-export default function Quiz(
+export default function Quiz(//Export default quiz function component
   {  // PROPS PASSED FROM PARENT COMPONENT
     questions, 
     currentQuestion, 
@@ -18,8 +20,10 @@ export default function Quiz(
     handleNextQuestion, 
     lastQuestion
   }
-
 ) {
+
+  //==================JSX RENDERING==================
+  
   return (
     <div>
       <Row>
