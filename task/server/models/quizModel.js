aa19874eb61c5586596cae7b696a6016a93e4766
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-07-12 08:37:58 +0200

    Update quizModel.js

--- task/server/models/quizModel.js
+++ task/server/models/quizModel.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose'); // Import the Mongoose library
 
-// Custom validation function to ensure each question has exactly 3 options
-function arrayLimit(val) {
-    return val.length === 3;
-}
-
-// Custom validation function to ensure each quiz has exactly 5 questions
-function arrayLimit5(val) {
-    return val.length === 5;
-}
 
 // Define the schema for quizzes
 const quizSchema = new mongoose.Schema({
@@ -39,6 +30,16 @@ const quizSchema = new mongoose.Schema({
         validate: [arrayLimit5, '{PATH} must have exactly 5 questions']
     }
 }, { timestamps: true });
+
+// Custom validation function to ensure each question has exactly 3 options
+function arrayLimit(val) {
+    return val.length === 3;
+}
+
+// Custom validation function to ensure each quiz has exactly 5 questions
+function arrayLimit5(val) {
+    return val.length === 5;
+}
 
 // Export the mongoose model for 'Quiz' using the defined schema
 module.exports = mongoose.model('quiz', quizSchema);
