356ce1a2af71d527e3a47040a334ba72dd360f51
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-08-10 12:52:40 +0200

    Update userSchema.js

--- Task-no-comments/server/models/userSchema.js
+++ Task-no-comments/server/models/userSchema.js
@@ -1,31 +1,25 @@
-// Import the Mongoose library
 const mongoose = require('mongoose');
 
 // Define the user schema
-let userSchema = new mongoose.Schema({
-    // Field for username
+let userSchema = new mongoose.Schema({   
     username:{
         type: String,
         required: [true, 'Username is required'],
         unique: true,
     },
-    // Field for user email
     email:{
         type: String,
         required: [true, 'User email is required'],
         unique: true,
     },
-    //Field for user date of birth
     dateOfBirth: {
         type: Date,
         required: [true, 'date of birth is required'],
     },
-    //Optional field for admin status
     admin: {
         type: Boolean,
         required: false,
     },
-    //Field for user password
     password: {
         type: String,
         required: [true, 'User password is required'],
