bef9b53ea9f616e0839d63a90614c269246a6fab
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-08-22 15:38:09 +0200

    Update quizModel.js

--- task/server/models/quizModel.js
+++ task/server/models/quizModel.js
@@ -33,30 +33,22 @@ const quizSchema = new mongoose.Schema({
                 options: {
                     type: [String],
                     required: true,
-                    validate: [arrayLimit, '{PATH} must have exactly 3 options']
+                    validate: [arrayLimit, '{PATH} must have exactly 3 options']// Custom validation function
                 }
             }
         ],
         required: true,
-        validate: [arrayLimit5, '{PATH} must have exactly 5 questions']
+        validate: [arrayLimit5, '{PATH} must have exactly 5 questions']// Custom validation function
     },
-    // Username of the person who created the quiz
-    username:{
+    /*// Username of the person who created the quiz
+    userId:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    
+        ref: 'User',// Reference to the User model
+        required: true,// Ensures that every quiz has a creator
+     },    */
 }, { timestamps: true });
 
-Quiz.findOne()
-    .populate('username')
-    .exec((err, username)=>{
-        if(!err){
-            console.log(username)
-        }
-        process.exit(0)
-    })
+
 // Custom validation function to ensure each question has exactly 3 options
 function arrayLimit(val) {
     return val.length === 3;
@@ -66,6 +58,17 @@ function arrayLimit(val) {
 function arrayLimit5(val) {
     return val.length === 5;
 }
+
+// Quiz.findOne()
+//     .populate('userId', 'username') // Populate the 'username' field from the 'User' model
+//     .exec((err, quiz) => {
+//         if (err) {
+//             console.error('Error populating username:', err);
+//         } else {
+//             console.log(quiz);
+//         }
+//         process.exit(0); // Exit the process once the query is complete
+//     });
 
 // Export the mongoose model for 'Quiz' using the defined schema
 module.exports = mongoose.model('Quiz', quizSchema);
