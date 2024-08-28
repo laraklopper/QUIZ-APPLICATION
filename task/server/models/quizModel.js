1d0743df2f68b936c09748cb879114056b9ec1b3
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-08-28 16:47:07 +0200

    Update quizModel.js

--- task/server/models/quizModel.js
+++ task/server/models/quizModel.js
@@ -17,6 +17,11 @@ const quizSchema = new mongoose.Schema({
     username: {
         type: String, 
         required: true, 
+    },
+    // Username of the person who created the quiz
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,// Reference to the User model
+        ref: 'User', // Ensures that every quiz has a creator
     },*/
     //Field for questions containing an array of objects
     questions: {
@@ -47,11 +52,7 @@ const quizSchema = new mongoose.Schema({
         // Custom validation function
         validate: [arrayLimit5, '{PATH} must have exactly 5 questions']
     },
-    /*// Username of the person who created the quiz
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,// Reference to the User model
-        ref: 'User', // Ensures that every quiz has a creator
-    },*/
+   
 }, { timestamps: true });
 // Automatically adds createdAt and updatedAt fields
 
@@ -67,18 +68,5 @@ function arrayLimit5(val) {
     return val.length === 5;
 }
 
-/*
-Quiz.findOne()
-// Populate the 'username' field from the 'User' model
-    .populate('userId', 'username') 
-    .exec((err, quiz) => {
-        if (err) {
-            console.error('Error populating username:', err);
-        } else {
-            console.log(quiz);
-        }
-        process.exit(0); // Exit the process once the query is complete
-    });
-*/
 // Export the mongoose model for 'Quiz' using the defined schema
 module.exports = mongoose.model('Quiz', quizSchema);
