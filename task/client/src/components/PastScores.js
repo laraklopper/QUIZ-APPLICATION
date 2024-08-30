cc6eeea3ba0baecd5e5c801f20b4e9be8d4928c3
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-08-30 14:25:59 +0200

    Add files via upload

---
+++ task/client/src/components/PastScores.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button from 'react-bootstrap/Button'; 
+
+
+//PastScores function component
+export default function PastScores({fetchScores, scores}) {
+  return (
+    <div>
+        {/* Map through past scores*/}
+        <Button variant='primary' onClick={fetchScores}>FETCH SCORES</Button>
+          {scores.length > 0 ? (
+              <DropdownButton id="dropdown-basic-button" title="PAST SCORES">
+          {scores.map((score, index) => (
+            <Dropdown.Item key={index}>{score.quizName}: {score.score}</Dropdown.Item>
+          ))}
+          </DropdownButton>
+          ):(
+            <p>NO SCORES available</p>
+          )}        
+    </div>
+  )
+}
