a146c088d33b0374a0705d40cf72dc3bae26dbc2
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-09 09:02:20 +0200

    Update App.js

--- Task-no-comments/client/src/App.js
+++ Task-no-comments/client/src/App.js
@@ -1,10 +1,10 @@
 // Import necessary modules and packages
 import React, { useEffect, useState, useCallback } from 'react';
-import './App.css';//Import CSS stylesheet
+import './App.css';
 // React Router components
 import {  Route, Routes, useNavigate } from 'react-router-dom';
 //Bootstrap
-import Container from 'react-bootstrap/Container';// Import Container component from Bootstrap
+import Container from 'react-bootstrap/Container';
 //Pages
 import Login from './pages/Login';
 import Registration from './pages/Registration';
@@ -17,102 +17,96 @@ import Page4 from './pages/Page4';
 export default function App() {
   //=======STATE VARIABLES===============
   //User variables
-  const [users, setUsers] = useState([]);//State used to store a list of all the users
-  const [currentUser, setCurrentUser] = useState(null);//State to store the user currently loggedIn
-  const [userData, setUserData] = useState({//State to store userData for login
-    username: '',        // Username entered in the login form
-    email: '',           // Email entered in the login form
-    dateOfBirth: '',     // Date of birth entered in the login form
-    admin: '',           // Admin status, if applicable
-    password: '',        // Password entered in the login form
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userData, setUserData] = useState({
+    username: '',
+    email: '',
+    dateOfBirth: '',
+    admin: '',
+    password: '',
   });
   //Quiz variables
-  const [quizList, setQuizList] = useState([]);//State to store the List of all quizzes
-  const [questions, setQuestions] = useState([]);//List of questions in the quit
-  const [quiz, setQuiz] = useState(null);//Currently selected quiz
-  const [quizName, setQuizName] = useState(''); // Name of the quiz
-  const [currentQuestion, setCurrentQuestion] = useState({// Current question being added
-    questionText: '',
-    correctAnswer: '',
-    options: ['', '', ''],
-  });
-  const [userScores, setUserScores] = useState({// State to store the current user's quiz scores
-    result: '',
-    date: '',//date format
-    attemptNumber: ''//number format
-  });
-  // const [scores, setScores] =useState([]);// State to hold scores
+    const [quizList, setQuizList] = useState([]);
+    const [questions, setQuestions] = useState([]);
+    const [quiz, setQuiz] = useState(null);
+    const [quizName, setQuizName] = useState('');
+    const [currentQuestion, setCurrentQuestion] = useState({
+      questionText: '',
+      correctAnswer: '',
+      options: ['', '', ''],
+    });
+    const [userScores, setUserScores] = useState({
+      result: '',
+      date: '',
+      attemptNumber: ''
+    });
+  // const [scores, setScores] =useState([]);
   // Event and UI-related states
-  const [error, setError] = useState(null); //Error message
-  const [loggedIn, setLoggedIn] = useState(false); //Boolean to track whether or not the user is currently logged in
-  const [selectedQuiz, setSelectedQuiz] = useState(null);// State to store the selected quiz
+  const [error, setError] = useState(null); 
+  const [loggedIn, setLoggedIn] = useState(false); 
+  const [selectedQuiz, setSelectedQuiz] = useState(null);
   // Hook to navigate between different routes
   const navigate = useNavigate()
+  
   //============USE EFFECT HOOK TO FETCH USERS======================
   //Fetch users when the component mounts or when loggedIn changes
   useEffect(() => {
     //Function to fetch users
-    const fetchUsers = async () => {
-      try {
-        // Retrieve JWT token from localStorage for authentication
-        const token = localStorage.getItem('token');
-        if (!token || !loggedIn) return;// If no token is found, exit the function
-
-      // Send a GET request to the server to fetch users
-         const response = await fetch('http://localhost:3001/users/findUsers', {
-           method: 'GET',//HTTP request methode
-           mode: 'cors',//Enable Cross-Origin Resource Sharing 
-           headers: {
-             'Content-Type': 'application/json',// Specify the content type for the request
-             'Authorization': `Bearer ${token}`,// Attach JWT token for authorization
-          }
-        });
-
-        //Conditional rendering to check if the response is not OK (status code not in the range 200-299) 
-        if (!response.ok) {
-          throw new Error('Failed to fetch users');//Throw an error message if the GET request is unsuccessful
-        }
-
-        // Parse the JSON data from the response body
-        const fetchedUsers = await response.json();
-        setUsers(fetchedUsers); 
-      } 
-      catch (error) {
-        console.error('Error fetching users', error.message);//Log an error message in the console for debugging purposes
-        setError('Error fetching users');//Set the error state with an error message
+   const fetchUsers = async () => {
+  try {
+    const token = localStorage.getItem('token');
+    if (!token || !loggedIn) return;
+    
+  // Send a GET request to the server to fetch users
+    const response = await fetch('http://localhost:3001/users/findUsers', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
       }
-    };
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch users');
+    }
+
+    const fetchedUsers = await response.json();
+    setUsers(fetchedUsers);
+  } catch (error) {
+    console.error('Error fetching users', error.message);
+    setError('Error fetching users');
+  }
+};
 
     //Function to fetch the current user details
-    const fetchCurrentUser = async () => {
-      try {
-        // Retrieve JWT token from localStorage for authentication
-        const token = localStorage.getItem('token');
-        if (!token) return;// If no token is found, exit the function
-        //Send a GET request to the server to fetch the current user id
-        const response = await fetch('http://localhost:3001/users/userId', {
-          method: 'GET',//HTTP request method
-          mode: 'cors',//Enable Cross-Origin resource sharing mode
-          headers: {
-            'Content-Type': 'application/json',// Specify the content type of the request
-            'Authorization': `Bearer ${token}`,// Attach JWT token for authorization
-          }
-        });
-        /* Conditional rendering to check if the response
-               is not successful (status code is not in the range 200-299)*/
-        if (!response.ok) {
-          throw new Error('Failed to fetch current user');//Throw an error message if the GET request is unsuccessful
-        }
+  const fetchCurrentUser = async () => {
+  try {
+    const token = localStorage.getItem('token');
+    if (!token) return;
+    
+    //Send a GET request to the server to fetch the current user id
+    const response = await fetch('http://localhost:3001/users/userId', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
+    });
 
-        // Parse the JSON data from the response body
-        const fetchedCurrentUser = await response.json();    // Parse and set the current user's details
-        setCurrentUser(fetchedCurrentUser);// Update state with fetched user details
-      }
-      catch (error) {
-        console.error('Error fetching current user', error.message);//Log an error message in the console for debugging purposes
-        setError('Error fetching current user');// Set the error state and an error messsage
-      }
-    };
+    if (!response.ok) {
+      throw new Error('Failed to fetch current user');
+    }
+
+    const fetchedCurrentUser = await response.json();
+    setCurrentUser(fetchedCurrentUser);
+  } catch (error) {
+    console.error('Error fetching current user', error.message);
+    setError('Error fetching current user');
+  }
+};
 
     //Conditional rendering to check if the user is logged in
     if (loggedIn) {
@@ -120,69 +114,56 @@ export default function App() {
       fetchCurrentUser();//Call the FetchCurrentUser function to fetch the current user's details
     }
   }, [loggedIn,  navigate/*, fetchUsers, fetchCurrentUser*/]);
-  // Dependency array:
-  // - `loggedIn`: The effect will run whenever the `loggedIn` state changes.
-  // - `navigate`: The effect will also run if the `navigate` function changes (though it's usually stable).
-  // - Including `fetchUsers` and `fetchCurrentUser` ensures that the effect re-runs if these functions change.
+ 
   //==============REQUESTS========================
   //-----------GET-------------------------
 // Function to fetch quizzes
   const fetchQuizzes = useCallback(async () => {
-    try {
-      // Retrieve the JWT token from localStorage
-      const token = localStorage.getItem('token');
-      
-      //Send a GET request to the server to find all quizzes
-      const response = await fetch('http://localhost:3001/quiz/findQuizzes', {
-        method: 'GET',//HTTP request method
-        mode: 'cors',// Enable Cross-Origin Resource Sharing
-        headers: {
-          'Content-Type': 'application/json',// Specify that the request and response are JSON
-          'Authorization': `Bearer ${token}`,// Attach JWT token for authorization
-        }
-      });
-
-      /* Conditional rendering to check if the response
-        is not successful (status code is not in the range 200-299)*/
-      if (!response.ok) {
-        throw new Error('Failed to fetch quizzes');//Throw an error message if the GET request is unsuccessful
+  try {
+    const token = localStorage.getItem('token');
+//Send a GET request to the server to find all quizzes
+    const response = await fetch('http://localhost:3001/quiz/findQuizzes', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
       }
-      const quizData = await response.json();// Parse the JSON data from the response body
+    });
 
-      //Conditional rendering to ensure the data is an array
-      if (quizData && Array.isArray(quizData.quizList)) {
-        setQuizList(quizData.quizList);// Update the quizList state with the fetched quiz data
-      } else {
-        throw new Error('Invalid quiz data');// Throw error if the data format is incorrect
-      }
-    
-    } 
-    catch (error) {
-      console.error('Error fetching quizzes:', error);
-      setError('Error fetching quizzes');
+    if (!response.ok) {
+      throw new Error('Failed to fetch quizzes');
     }
-  },[]);
-  /* The useCallback hook ensures that the function is not 
+
+    const quizData = await response.json();
+
+    if (quizData && Array.isArray(quizData.quizList)) {
+      setQuizList(quizData.quizList);
+    } else {
+      throw new Error('Invalid quiz data');
+    }
+
+  } catch (error) {
+    console.error('Error fetching quizzes:', error);
+    setError('Error fetching quizzes');
+  }
+}, []);
+ /* The useCallback hook ensures that the function is not 
   recreated on every render*/
 
-  //Function to fetch a users score
+ 
 
 //===========EVENT LISTENERS============================
   // Function to handle user logout
-  /* useCallback hook is to handle user logout, 
-  memoized to avoid unnecessary re-renders*/
-  const logout = useCallback (() => {
-    // Remove JWT token from localStorage to clear authentication
-    localStorage.removeItem('token');
-    localStorage.removeItem('username');// Remove username from localStorage
-    localStorage.removeItem('loggedIn');  // Remove logged-in status from localStorage
-    // Update local state to reflect that the user is no longer logged in
-    setLoggedIn(false);
-    setError(''); // Clear any existing error messages
-    setUserData({ username: '', password: '' });//Reset the userData
-    //Use the navigate function to redirect the user to the home page after logging out
-    navigate('/');
-  }, [navigate]);
+ const logout = useCallback(() => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('username');
+  localStorage.removeItem('loggedIn');
+  setLoggedIn(false);
+  setError('');
+  setUserData({ username: '', password: '' });
+  navigate('/');
+}, [navigate]);
 
   //===========JSX RENDERING=============================
 
