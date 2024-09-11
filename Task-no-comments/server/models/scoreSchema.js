a836afbd3ef287c316b6487143d8c7a7b5a5b3be
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-11 17:27:27 +0200

    Update scoreSchema.js

--- Task-no-comments/server/models/scoreSchema.js
+++ Task-no-comments/server/models/scoreSchema.js
@@ -1,54 +1,69 @@
 // Import necessary modules and packages
-// Import the Mongoose library
 const mongoose = require('mongoose');
-// Import the autopopulate plugin for Mongoose
 const autopopulate = require('mongoose-autopopulate');
 
 //Define the score schema
 const scoreSchema = new mongoose.Schema({
-    /* Field for username of the user who
+      /* Field for username of the user who
     took the quiz*/
     userId: {
-        type: mongoose.Schema.Types.ObjectId,// Define the type as ObjectId
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'User',// Reference the User collection
         autopopulate: true,// Automatically populate the quiz field
-        required: true,//Indicate that the username is required
-        /*Validate to ensure the quiz is a valid objectId*/
+        required: true,
+        /*Validate to ensure the username is a valid objectId*/
         validate: {
-            validator: mongoose.Types.ObjectId.isValid,// Validate the ObjectId format
+            validator: mongoose.Types.ObjectId.isValid,
             message: 'Invalid user Id',//Custom error message if validation fails
         },
-    },
+    }, 
+    /*username: {
+        type: String,
+        required: true,
+    },*/
     // Field for the name of the quiz taken
     quizId: {
-        type: mongoose.Schema.Types.ObjectId,// Define the type as ObjectId
-        ref: 'Quiz',// Reference the Quiz collection
+        type: mongoose.Schema.Types.ObjectId,
+        // Reference the Quiz collection
+        ref: 'Quiz',
         autopopulate: true,// Automatically populate the quiz field
-        required: [true, 'Quiz name is required'],//Indicate that the quizName is required
+        required: [true, 'Quiz name is required'],
         /*Validate to ensure the username is a valid objectId*/
-        validate:{
-            validator: mongoose.Types.ObjectId.isValid,// Validate the ObjectId format
+        validate: {
+            validator: mongoose.Types.ObjectId.isValid,
             message: 'Invalid quiz Id',//Custom error message if validation fails
         },
     },
+    /*name: {
+        type: String,
+        required:[true, 'Quiz name is required'],   
+    },
+    */
+    //Field for the score
     score: {
-        type: Number,  // Define the type as Number
-        default: 0,// Default score value
-        required: true, //Indicate that the score is required
-        set: (v) => Math.floor(v),//Ensure the score is an integer
+        type: Number,// Define the type as Number
+        default: 0,
+        required: true,
+        //Ensure the score is an integer
+        set: (v) => Math.floor(v),
         /*Validate to ensure the score is a valid integer*/
         validate: {
-            validator: Number.isInteger, // Validate that score is an integer
+            validator: Number.isInteger,
             message: 'Score must be an integer',//Custom error message if validation fails
         },
     },
-    //Field for the number of times the user has attempted the quiz
+     //Field for the number of times the user has attempted the quiz
     attempts: {
-        type: Number,  // Define the type as Number
-        default: 0,//Default value for attempts
-        // required: true,   //State that the number of attempts is required
-        set: (v) => Math.floor(v), // Ensure attempts is an integer
-    }, 
+        type: Number,  
+        default: 0,
+        set: (v) => Math.floor(v),
+    },
+    //Field for the date of the current attempt
+    date: {
+        type: Date,//Define the dataType as a date
+        default: Date.now,
+        required: true,
+    },
 }, {timestamps: true})
 // Automatically adds createdAt and updatedAt fields
 
