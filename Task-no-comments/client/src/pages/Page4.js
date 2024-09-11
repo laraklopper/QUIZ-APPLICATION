e0ed6a2b35b4771a46463f9d8e977a3aaf57771a
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-11 16:04:20 +0200

    Update Page4.js

--- Task-no-comments/client/src/pages/Page4.js
+++ Task-no-comments/client/src/pages/Page4.js
@@ -6,73 +6,69 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 //Components
-import Header from '../components/Header';//Import the Header function component
-import EditUser from '../components/EditUser';//Import the EditUser function component
-import Footer from '../components/Footer';//Import the Footer function component
-import UserViewBtn from '../components/UserViewBtn';//Import the UserViewBtn function component
+import Header from '../components/Header';
+import EditUser from '../components/EditUser';
+import Footer from '../components/Footer';
+import UserViewBtn from '../components/UserViewBtn';
 
 // Page 4 function component
-export default function Page4({
-  // PROPS PASSED FROM PARENT COMPONENT
+export default function Page4(//Export default Page4 function component
+  {// PROPS PASSED FROM PARENT COMPONENT
   logout, 
   currentUser, 
   setUsers, 
   setLoggedIn, 
   setError, 
   users
-}) {
+}
+) {
   //===========STATE VARIABLES===============
-  const [editUserData, setEditUserData] = useState({ // State for user edit form data
+  const [editUserData, setEditUserData] = useState({ 
     editUsername: '',
     editUserEmail: ''
   });
-  const [updateUser, setUpdateUser] = useState(null);  // State for tracking which user's details are being updated
-  const [viewUsers, setViewUsers] = useState(false);// State for toggling user list visibility
+  const [updateUser, setUpdateUser] = useState(null);  
+  const [viewUsers, setViewUsers] = useState(false);
 
   //---------------------------------------
-
   // Function to specify the date format
   const dateDisplay = (dateString) => {
-    // Define the format options for day, month, and year
     const options = {
       day: '2-digit',
       month: '2-digit',
       year: 'numeric'
     };
-    // Convert the input date string to a Date object and format it using the options
     return new Date(dateString).toLocaleDateString('en-GB', options);
   }
 
-  //======REQUESTS===============
-
-  
+  //======REQUESTS===============  
   //--------PUT---------------
   // Function to edit a user 
-  const editUser = async (userId) => {
+  const editUser = async (userId) => {//Define an async funcion to edit User details
     try {
-      const token = localStorage.getItem('token');// Retrieve the authentication token from local storage
+      const token = localStorage.getItem('token');
       if (!token) {
-        throw new Error('No token available');// Throw an error if token is not found
+        throw new Error('No token available');
       }
       // Send PUT request to the server to update user details
       const response = await fetch(`http://localhost:3001/users/editAccount/${userId}`, {
-        method: 'PUT',//HTTP request method
-        mode: 'cors',// Enable Cross-origin resource sharing
+        method: 'PUT',
+        mode: 'cors',
         headers: {
           'Content-Type': 'application/json',// Set content type to JSON
           'Authorization': `Bearer ${token}`,// Include token for authentication
         },
         body: JSON.stringify({
-          username: editUserData.editUsername,// Updated username
-          email: editUserData.editUserEmail,// Updated email
+          username: editUserData.editUsername,
+          email: editUserData.editUserEmail,
         })
       });
 
-      // Conditional rendering to che if the response is OK (status code 200-299)
+      // Response handling
       if (!response.ok) {
-        throw new Error('Failed to update user account');// Throw an error if response is not OK
+        throw new Error('Failed to update user account');
       }
-      const updatedAccount = await response.json(); // Parse the updated account data from the response
+      const updatedAccount = await response.json(); 
 
       // Update the list of users in the state with the modified user details
       setUsers((prevUsers) =>
@@ -82,73 +78,64 @@ export default function Page4({
       // Reset the edit form data and close the update form
       setEditUserData({ editUsername: '', editUserEmail: '' });
       setUpdateUser(null);
-      setLoggedIn(true);// Optionally, refresh user login status
+      setLoggedIn(true);
 
-      console.log('User account successfully updated');// Log success message in the console for debugging purposes
+      console.log('User account successfully updated');
     } catch (error) {
-      console.error(`Error updating user account: ${error.message}`);//Log an error message in the console for debugging purposes
-      setError(`Error updating user account: ${error.message}`);// Set the error state to display the error in the UI
+      console.error(`Error updating user account: ${error.message}`);
+      setError(`Error updating user account: ${error.message}`);
     }
   };
 
   //-----------DELETE----------------------
   // Function to remove a user
-  const deleteUser = async (userId) => {
+   const deleteUser = async (userId) => {//Define an async function to delete a user
     try {
-      // Send delete request to server
+      // Send a DELETE request to server
       const response = await fetch(`http://localhost:3001/users/deleteUser/${userId}`, {
-        method: 'DELETE',//HTTP request method
-        mode: 'cors',//Enable Cross-Origin resource sharing
+        method: 'DELETE',
+        mode: 'cors',
         headers: {
           'Content-type': 'application/json',//Specify the Content-Type as JSON
         },
       });
 
-      // Conditional rendering if the response is OK (status code 200-299)
+      // Response handling
       if (!response.ok) {
-        throw new Error('Failed to remove user');// Throw an error if the DELETE request is unsuccessful
+        throw new Error('Failed to remove user');
       }
       // Update the user list in the state to remove the deleted user
       setUsers((prevUsers) =>
         prevUsers.filter((user) => user._id !== userId)
       );
-      console.log('User Successfully removed');// Log a success message in the console for debugging purposes
+      console.log('User Successfully removed');
 
     } catch (error) {
-      setError('Error removing user', error);// Set the error state to display the error in the UI
-      console.error('Error removing user:', error.message);//Log an error message in the console for debugging purposes
+      setError('Error removing user', error);
+      console.error('Error removing user:', error.message);
     }
   };
 
   //==========EVENT LISTENERS=============
-  // Function to handle input change in the update form
   const handleInputChange = (event) => {
     // Extract name and value from the event target
     const { name, value } = event.target;
     setEditUserData((prevData) => ({
       ...prevData,
-      [name]: value, // Update the property based on the input name
+      [name]: value, 
     }));
   };
 
   // Function to toggle the update form
   const updateAccount = (userId) => {
-    // Toggle the current update form based on userId
-    // If the current userId matches the state, set it to null (hide form)
-    // Otherwise, set the state to the new userId (show form)
     setUpdateUser(userId === updateUser ? null : userId);
   };
 
   /* Function to toggle the display users list if 
      the current user is an admin user */
-  const toggleViewUsers = () => {
-    /* Conditional rendering to check if the 
-    current user is an admin user*/
-    
+  const toggleViewUsers = () => {    
     if (currentUser.admin) {
-      setViewUsers(!viewUsers);// Toggle the visibility of the user list
-      /*Admin users must only be able to view and 
-      delete non-admin users*/
+      setViewUsers(!viewUsers); 
     } else {
       console.log('Unauthorized');
       alert('Unauthorized');
@@ -157,7 +144,7 @@ export default function Page4({
 
   //=======JSX RENDERING==================
 
-  return (
+ return (
     <>
       {/* Header */}
       <Header heading='USER ACCOUNT' />
