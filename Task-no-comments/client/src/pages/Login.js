ff4a35b905370b39ecd4ae5ae7095bc25dfec930
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-11 15:37:05 +0200

    Update Login.js

--- Task-no-comments/client/src/pages/Login.js
+++ Task-no-comments/client/src/pages/Login.js
@@ -1,6 +1,6 @@
 // Import necessary modules and packages
-import React, { useState } from 'react';// Import the React module to use React functionalities
-import '../CSS/login.css'; // Import CSS stylesheet for login page styling
+import React, { useState } from 'react';
+import '../CSS/login.css'; 
 //Components
 import MainHeader from '../components/MainHeader';
 import PageFooter from '../components/PageFooter';
@@ -15,59 +15,58 @@ export default function Login(
         setLoggedIn
     }) {
       //=========STATE VARIABLES===============
-  // Boolean to control the visibility of the password field
   const [showPassword, setShowPassword] = useState(false);
 
       //=============REQUESTS==================
       //-------------POST------------------------------
       //Function to submitLogin
-  const submitLogin = async () => {
+   const submitLogin = async () => {//Define an async function to submitLogin
     try {
       //Send a POST request to the server
       const response = await fetch('http://localhost:3001/users/login', {
-        method: 'POST',//HTTP request method
-        mode: 'cors',// Enable Cross-Origin Resource Sharing
+        method: 'POST',
+        mode: 'cors',
         headers: {
-          'Content-Type': 'application/json',// Specify the request body is JSON
+          'Content-Type': 'application/json',// Specify the Content-Type
         },
-        body: JSON.stringify({// Convert userData object to JSON string for the request body
+        body: JSON.stringify({
           username: userData.username,
           password: userData.password
         })
       });
 
-      /* Conditional rendering to check if the response is successful 
-      (status code in the range 200-299)*/
+        //Response handling
       if (response.ok) {
-        const data = await response.json();// Parse the JSON response from the server
+        const data = await response.json();
         // Store user details and token in localStorage
         localStorage.setItem('username', userData.username);
         localStorage.setItem('loggedIn', true);
         localStorage.setItem('token', data.token);
-        // Update local state to indicate the user is logged in
         setLoggedIn(true);
-        setError(null);// Clear any previous errors
+        setError(null);
         // Clear user data from state
         setUserData({
           username: '',
           password: '',
         });
-      } else {
-        throw new Error('Username or password are incorrect');// Throw error message if the POST request is unsuccessful
+      } 
+      else {
+        throw new Error('Username or password are incorrect');
       }
     } catch (error) {
       // Handle errors during the login process
-      setError(`Login Failed: ${error.message}`);//Set the error State with an error message
-      console.log(`Login Failed: ${error.message}`);//Log an error message in the console for debugging purposes
-      setLoggedIn(false);// Ensure loggedIn state is set to false
+      setError(`Login Failed: ${error.message}`);
+      console.error(`Login Failed: ${error.message}`);
+      setLoggedIn(false);
       alert('LOGIN FAILED');// Notify user of failure
     }
   };
  
  
+ 
     //======JSX RENDERING=========
 
-  return (
+return (
     <>
     {/* Main Header Component */}
     <MainHeader mainHeading='LOGIN'/>
