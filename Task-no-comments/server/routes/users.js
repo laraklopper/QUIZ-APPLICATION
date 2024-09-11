16143611d4c63c5fcbaa52ff325b0e736e8cfd88
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-11 15:19:10 +0200

    Update users.js

--- Task-no-comments/server/routes/users.js
+++ Task-no-comments/server/routes/users.js
@@ -9,33 +9,30 @@ const User = require('../models/userSchema');
 // const {authenticateToken, checkAge} = require('./middleware');
 
 //=======SETUP MIDDLEWARE===========
-router.use(cors()); // Enable Cross-Origin Resource Sharing
-router.use(express.json()); // Parse incoming JSON requests
+router.use(cors());
+router.use(express.json()); 
 
 //=========CUSTOM MIDDLEWARE==================
 
 // Middleware to verify JWT and extract user info
 const authenticateToken = (req, res, next) => {
-    // Extract the authorization header
     const authHeader = req.headers['authorization'];
-    // Extract the token from the header
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);// If no token, return unauthorized
+    if (token == null) return res.sendStatus(401);
 
     // Verify the token using a secret key
     jwt.verify(token,
-        'secretKey',//SecretKey
+        'secretKey',
         (err, user) => {
-            if (err) return res.sendStatus(403);// If token invalid, return forbidden
-            req.user = user;// Attach user info to request object
-            next();// Proceed to the next middleware or route handler
+            if (err) return res.sendStatus(403);
+            req.user = user;
+            next();
         });
 };
 
 //Middleware function to check that admin user is 18 years or older
 const checkAge = (req, res, next) => {
-    // Extract the date of birth from the request body
     const { dateOfBirth } = req.body;
 
     if (!dateOfBirth) {
@@ -55,24 +52,10 @@ const checkAge = (req, res, next) => {
             { error: 'Admin users must be above 18 years old' }
         );
     }
-    // If age is valid, proceed to the next middleware or route handle
     next();
 };
 
 //=============ROUTES=====================
-/*
-|================================================|
-| CRUD OPERATION | HTTP VERB | EXPRESS METHOD    |
-|================|===========|===================|
-|CREATE          | POST      |  router.post()    |
-|----------------|-----------|-------------------|
-|READ            | GET       |  router.get()     |  
-|----------------|-----------|-------------------|     
-|UPDATE          | PUT       |  router.put()     |
-|----------------|-----------|-------------------|
-|DELETE          | DELETE    |  router.delete()  |
-|================|===========|===================|
-*/
 //------------------GET---------------
 //Route to handle GET requests to fetch a single user
 router.get('/userId',authenticateToken, async (req, res) => {
@@ -84,49 +67,40 @@ router.get('/userId',authenticateToken, async (req, res) => {
         
         //Conditional rendering to check if the user exists
          if (!user) {
-             console.error('User not found');//Log an error message in the console for debugging purposes
-             return res.status(400).json(// If no user is found, log an error and send a 400 response
-                { message: 'User Not Found'}
-            )
+             console.error('User not found');
+             return res.status(400).json({ message: 'User Not Found'})
          }
-
+        
         res.status(200).json(user); // Return the users in the response
-        console.log('User details', user);//Log the user details in the console for debugging purposes
-         
+        console.log('User details', user);        
     } 
     catch (error) {
-        console.error('Error fetching Users', error.message);//Log an error message in the console for debugging purpose
-        res.status(500).json(//send a 500 response if something goes wrong
-            {message: 'Internal Server Error', error: error.message}
-        )
+        console.error('Error fetching Users', error.message);
+        res.status(500).json({message: 'Internal Server Error', error: error.message})          
     }
 })
-
 
 //Route to GET all users
 router.get('/findUsers', async (req, res) => {
-    // console.log('Finding Users');//Log a message in the console for debugging purposes
+    // console.log('Finding Users');
     try {
         
-        const { username } = req.query;// Extract the username from the query parameters
-        // If a username is provided, use it to filter users, otherwise return all users
+        const { username } = req.query;
         const query = username ? { username } : {};
-        const users = await User.find(query); // Fetch users based on the query object
+        const users = await User.find(query); 
     
-        console.log(users);// Log the fetched users for debugging purposes
+        console.log(users);
         res.status(200).json(users);// Send the list of users as the response
     } 
     catch (error) {
-        console.error('Error fetching users', error.message);//Log an error message in the console for debugging purpose
-        res.status(500).json(//send a 500 response if something goes wrong
-            { message: 'Internal server Error' }
-        );
+        console.error('Error fetching users', error.message);
+        res.status(500).json({ message: 'Internal server Error' });
     }
 })
 
 
+
 //-------------POST-------------------
-//Route to send POST request to login endpoint
 router.post('/login', async (req, res) => {
     console.log(req.body);//Log the request body in the console for debugging purposes
     // console.log('User Login')//Log a message in the console for debugging purposes
@@ -146,34 +120,30 @@ router.post('/login', async (req, res) => {
         if (username === user.username && password === user.password) {
             // Generate a JWT token for authentication
             const jwtToken = jwt.sign(
-                { userId: user._id },// Payload containing user ID
-                     'secretKey',// Secret key for token signing
-                        /*process.env.JWT_SECRET,*/
+                { userId: user._id },
+                     'secretKey',
+                    /*process.env.JWT_SECRET,*/
                         {
-                            expiresIn: '12h',// Token expiration time
-                            algorithm: 'HS256',// Signing algorithm
+                            expiresIn: '12h',
+                            algorithm: 'HS256',
                         }
                     );
             // Send the generated JWT token as a JSON response
-            res.json({ 'token': jwtToken })//Send the generated token back to the client
+            res.json({ 'token': jwtToken })
         } 
         else {
-            //Throw an error message if the user credentials are incorrect
             throw new Error('Password Incorrect');
         }
-     
     } catch (error) {
-        console.error('Login Failed: Username or password are incorrect');//Log an error message in the console for debugging purposes
-        res.status(401).json(//Send a 401 status error message to the client indicating an authentication failure
-            { message: 'User not authenticated' })
+        console.error('Login Failed: Username or password are incorrect');
+        res.status(401).json({ message: 'User not authenticated' })            
     }
 })
 
 //Route to send a POST request the register endpoint
 router.post('/register', checkAge, async (req, res) => {
-    // console.log(req.body);//Log the request body in the console for debugging purposes
+    // console.log(req.body);
     try {
-        // Extract user details from the request body, with 'admin' defaulting to false
         const { username, email, dateOfBirth, password, admin = false } = req.body;
         
       
@@ -183,7 +153,6 @@ router.post('/register', checkAge, async (req, res) => {
                 { message: 'Username, email, date of birth, and password are required' });
         }
       
-        // Conditional rendering to check if a user with the same username already exists
         const existingUser = await User.findOne({ username });
 
         if (existingUser) {
@@ -206,12 +175,12 @@ router.post('/register', checkAge, async (req, res) => {
 
         // Generate JWT token for the newly registered user
         const token = jwt.sign(
-            { _id: savedUser._id },// Payload containing the user's ID
-            'secretKey', // Secret key used to sign the token
+            { _id: savedUser._id },
+            'secretKey', 
             /*process.env.JWT_SECRET,*/
             {
-                expiresIn: '12h',// Token expiration time set to 12 hours
-                algorithm: 'HS256'// Specify the signing algorithm 
+                expiresIn: '12h',
+                algorithm: 'HS256'
             }
         );
 
@@ -221,20 +190,17 @@ router.post('/register', checkAge, async (req, res) => {
 
     } 
     catch (error) {
-        console.error('Failed to add User');//Log an error message in the console for debugging purpose
+        console.error('Failed to add User');
         return res.status(500).json({ error: 'Internal Server Error' })
     }
 })
 
-
 //-------------PUT-----------------
 //Route to edit a user account
 router.put('/editAccount/:id', async (req, res) => {
-    console.log('edit Account');// Log a message in the console for debugging purposes
+    // console.log('edit Account');
     try {        
-        // Extract the user ID from the request parameters
         const { id } = req.params;
-        // Extract the username and email from the request body
         const { username, email } = req.body;
 
 
@@ -245,13 +211,13 @@ router.put('/editAccount/:id', async (req, res) => {
 
         // Find the user by ID and update the relevant fields
         const updatedAccount = await User.findByIdAndUpdate(
-            id, // The ID of the user to update
-            updateUser, // The fields to update
-            { new: true } // Return the updated document
+            id, 
+            updateUser, 
+            { new: true } 
         );
 
 
-        //Conditional rendering to check if the updated user is found
+    
         if (!updatedAccount) {
             return res.status(404).json({ message: 'User not found' })
         }
@@ -275,19 +241,18 @@ router.put('/editAccount/:id', async (req, res) => {
 //Route to send a DELETE request to the /deleteUser endpoint
 router.delete('/deleteUser/:id', async (req, res) => {
     try {
-        // Extract the user ID from the request parameters
         const { id } = req.params; 
-        // Find the user by ID and delete the document
+        // Find the user by ID and delete the document 
         const removedUser = await User.findByIdAndDelete(id);
-
+        
         if (!removedUser) {                    
             return res.status(404).json({ message: 'User not found' });
         }
+        //console.log(removedUser)
         res.json({
             message: 'User Successfully deleted',
             deletedUserId: removedUser._id
         })
-
     } 
     catch (error) {
         console.error('Error deleting user:', error.message);
