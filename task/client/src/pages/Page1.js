ed360ad743d04999d572217e8ef71a1101f7b674
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-13 16:47:55 +0200

    Update Page1.js

--- task/client/src/pages/Page1.js
+++ task/client/src/pages/Page1.js
@@ -1,18 +1,18 @@
 // Import necessary modules and packages
-import React, {useEffect, useCallback} from 'react';
-import '../CSS/Page1.css'
+import React, { useEffect, useCallback } from 'react';// Import the React module to use React functionalities
+import '../CSS/Page1.css'//Import CSS stylesheet
 //Bootstrap
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row'; // Import the Row component from react-bootstrap
+import Col from 'react-bootstrap/Col'; // Import the Col component from react-bootstrap
+import Dropdown from 'react-bootstrap/Dropdown';// Import the Dropdown component from react-bootstrap
+import DropdownButton from 'react-bootstrap/DropdownButton';// Import the DropdownButton component from react-bootstrap
+import Button from 'react-bootstrap/Button'; // Import the Button component from react-bootstrap
 //Components
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../components/Header';//Import the Header function component
+import Footer from '../components/Footer';//Import the Footer function component
 
 //Page1 function component
-export default function Page1(
+export default function Page1(//Export default Page1 function component
   { //PROPS PASSED FROM PARENT COMPONENT 
     logout,
     setError,
@@ -29,45 +29,49 @@ export default function Page1(
 //Function to display the Scores list from the database
 const fetchScores = useCallback(async () => {//Define an async function to fetch userScores
   try {
-    const token =localStorage.getItem('token')
+    const token =localStorage.getItem('token'); //Retrieve the token from localStorage
+    const username = localStorage.getItem('username');//Retrieve the username from localStorage
     //Send a GET request to the server to find scores
-    const response = await fetch (`http://localhost:3001/scores/findScores`, {
-      method: 'GET',
-      mode: 'cors',
+    const response = await fetch (`http://localhost:3001/scores/findScores/${username}`, {
+      method: 'GET',//HTTP request method
+      mode: 'cors',//Enable Cors for cross-origin resourcing
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',// Specify the Content-Type 
+        'Authorization': `Bearer ${token}`,// Authorization header 
       }
     })
 
-   
+    /* Conditional rendering to check if the response
+       is not successful (status code is not in the range 200-299)*/
     if (!response.ok) {
       // Throw an error if GET request status is not OK(unsuccessful)
       throw new Error('Unable to fetch user scores');
     }
 
-    const quizScores = await response.json(); 
+    const quizScores = await response.json(); // Parse the response JSON data
 
+    // Conditional rendering to check if the fetched data is valid and an array
     if (quizScores && Array.isArray(quizScores.userScores)) {
-      setUserScores(quizScores.userScores); 
+      setUserScores(quizScores.userScores); // Update state with fetched scores
     } 
     else {
-      throw new Error('Invalid data type');
+      throw new Error('Invalid data type');//Throw an error message if the data type is invalid
     }
     
   } catch (error) {
-    console.error('Error fetching userScores', error.message);
-    setError(`Error fetching userScores: ${error.message}`);  
+    console.error('Error fetching userScores', error.message);//Log an error message in the console for debugging purposes
+    setError(`Error fetching userScores: ${error.message}`);  // Set the error state and an error messsage
   }
 },[setUserScores, setError])
 
-  // Filter the results by the selected quiz name
+ // Filter the results by the selected quiz name
   const quizResults = selectedQuiz
-    ? userScores.filter(score => score.quizName === selectedQuiz) 
+    ? userScores.filter(score => score.quizName === selectedQuiz)  // Filter the scores based on the selected quiz
     : [];
 
+  // useEffect hook to fetch scores when the component mounts
     useEffect (() => {
-      fetchScores()
+      fetchScores()// Call the fetchScores function 
     },[fetchScores])
 //========JSX RENDERING================
 
