d0a3816b9123df4b971813e946550ac850268983
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-17 16:24:18 +0200

    Update Login.js

--- task/client/src/pages/Login.js
+++ task/client/src/pages/Login.js
@@ -7,7 +7,7 @@ import PageFooter from '../components/PageFooter';//Import the PageFooter functi
 import LoginForm from '../components/LoginForm';//Import LoginForm function component
 
 //Login function component
-export default function Login(
+export default function Login(//Export the default Login function component
     {//PROPS PASSED FROM PARENT COMPONENT
         userData, 
         setUserData,
@@ -15,21 +15,21 @@ export default function Login(
         setLoggedIn
     }) {
       //=========STATE VARIABLES===============
-  const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);//State to display password visibility
 
       //=============REQUESTS==================
       //-------------POST------------------------------
       //Function to submitLogin
   const submitLogin = async () => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem('token');//Retrieve JWT token from localStorage
       //Send a POST request to the server
       const response = await fetch('http://localhost:3001/users/login', {
-        method: 'POST',
-        mode: 'cors',
+        method: 'POST',//HTTP request method
+        mode: 'cors',// Enable CORS for cross-origin requests
         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',// Specify the Content-Type 
+          'Authorization': `Bearer ${token}`,// Authorization header 
         },
         body: JSON.stringify({
           username: userData.username,
@@ -37,12 +37,13 @@ export default function Login(
         })
       });
 
-   
+     /* Conditional rendering to check if the response
+       is not successful (status code is not in the range 200-299)*/
       if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem('username', userData.username);
-        localStorage.setItem('loggedIn', true);
-        localStorage.setItem('token', data.token);
+        const data = await response.json();  // Parse the response data as JSON
+        localStorage.setItem('username', userData.username); // Save username
+        localStorage.setItem('loggedIn', true); // Set login status to true
+        localStorage.setItem('token', data.token); // Store received JWT token
         // Update local state to indicate the user is logged in
         setLoggedIn(true);
         setError(null);// Clear any previous errors
@@ -58,7 +59,7 @@ export default function Login(
       // Handle errors during the login process
       setError(`Login Failed: ${error.message}`);//Set the error State with an error message
       console.log(`Login Failed: ${error.message}`);//Log an error message in the console for debugging purposes
-      setLoggedIn(false);// Ensure loggedIn state is set to false
+      setLoggedIn(false);// Set login status to false in case of failure
       alert('LOGIN FAILED');// Notify user of failure
     }
   };
