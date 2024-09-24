c0cad49c6606563f79fd9d79327d6f3b33d373b5
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-24 10:34:48 +0200

    Update Page4.js

--- Task-no-comments/client/src/pages/Page4.js
+++ Task-no-comments/client/src/pages/Page4.js
@@ -1,16 +1,17 @@
 // Import necessary modules and packages
-import React, { useState } from 'react';// Import the React module to use React functionalities
-import '../CSS/Page4.css';//Import CSS stylesheet
+// Import the React module to use React functionalities
+import React, { useState } from 'react';
+import '../CSS/Page4.css';
 //Bootstrap
-import Row from 'react-bootstrap/Row'; // Import the Row component from react-bootstrap
-import Col from 'react-bootstrap/Col'; // Import the Col component from react-bootstrap
-import Button from 'react-bootstrap/Button'; // Import the Button component from react-bootstrap
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 //Components
-import Header from '../components/Header';//Import the Header function component
-import EditUser from '../components/EditUser';//Import the EditUser function component
-import Footer from '../components/Footer';//Import the Footer function component
-import UserViewBtn from '../components/UserViewBtn';//Import the UserViewBtn function component
+import Header from '../components/Header';
+import EditUser from '../components/EditUser';
+import Footer from '../components/Footer';
+import UserViewBtn from '../components/UserViewBtn';
+
 
 // Page 4 function component
 export default function Page4({//Export default Page4 function component
@@ -23,13 +24,13 @@ export default function Page4({//Export default Page4 function component
   users
 }) {
   //===========STATE VARIABLES===============
-  const [editUserData, setEditUserData] = useState({ // State for user edit form data
-    editUsername: '',
-    editUserEmail: ''
-  });
-  const [updateUser, setUpdateUser] = useState(null);  // State for tracking which user's details are being updated
-  const [viewUsers, setViewUsers] = useState(false);// State for toggling user list visibility
-  const [isUnauthorized, setIsUnauthorized] = useState(false); // State for userView authorisation access
+const [editUserData, setEditUserData] = useState({
+  editUsername: '',
+  editUserEmail: ''
+});
+const [updateUser, setUpdateUser] = useState(null);
+const [viewUsers, setViewUsers] = useState(false);
+const [isUnauthorized, setIsUnauthorized] = useState(false);
 
   //---------------------------------------
 
@@ -48,32 +49,34 @@ export default function Page4({//Export default Page4 function component
   //======REQUESTS===============
   //--------PUT---------------
   // Function to edit a user 
-  const editUser = async (userId) => {//Define an async funcion to edit User details
+  const editUser = async (userId) => {
     try {
-      const token = localStorage.getItem('token');//Retrieve the JWT token from localStorage
-      //Conditional rendering to check if the JWT token is found
+      //Retrieve the JWT token from localStorage
+      const token = localStorage.getItem('token');
+     
       if (!token) {
-        throw new Error('No token available');//Throw an error message if no token is available 
+        throw new Error('No token available');
       }
+      
       // Send PUT request to the server to update user details
       const response = await fetch(`http://localhost:3001/users/editAccount/${userId}`, {
         method: 'PUT',//HTTP request method
-        mode: 'cors',// Enable Cross-origin resource sharing
+        mode: 'cors',
         headers: {
-          'Content-Type': 'application/json',//Specify the Content-Type in the payload as JSON
+          'Content-Type': 'application/json',//Specify the Content-Type
           'Authorization': `Bearer ${token}`,// Include token for authentication
         },
         body: JSON.stringify({
-          username: editUserData.editUsername,// Updated username
-          email: editUserData.editUserEmail,// Updated email
+          username: editUserData.editUsername,
+          email: editUserData.editUserEmail,
         })
       });
 
-      // Conditional rendering to check if the response is OK (status code 200-299)
+      //Response handling
       if (!response.ok) {
-        throw new Error('Failed to update user account');// Throw an error if response is not OK
+        throw new Error('Failed to update user account');
       }
-      const updatedAccount = await response.json(); // Parse the updated account data from the response
+      const updatedAccount = await response.json(); 
 
       // Update the list of users in the state with the modified user details
       setUsers((prevUsers) =>
@@ -85,41 +88,42 @@ export default function Page4({//Export default Page4 function component
       setUpdateUser(null);
       setLoggedIn(true);// Optionally, refresh user login status
 
-      console.log('User account successfully updated');// Log success message in the console for debugging purposes
+      console.log('User account successfully updated');
     } catch (error) {
-      console.error(`Error updating user account: ${error.message}`);//Log an error message in the console for debugging purposes
-      setError(`Error updating user account: ${error.message}`);// Set the error state to display the error in the UI
+      console.error(`Error updating user account: ${error.message}`);
+      setError(`Error updating user account: ${error.message}`);
     }
   };
 
   //-----------DELETE----------------------
   // Function to remove a user
-  const deleteUser = async (userId) => {//Define an async function to delete a user
+  const deleteUser = async (userId) => {
     try {
+      // Retrieve token from localStorage
       const token = localStorage.getItem('token')
       // Send a DELETE request to server
       const response = await fetch(`http://localhost:3001/users/deleteUser/${userId}`, {
         method: 'DELETE',//HTTP request method
-        mode: 'cors',//Enable Cross-Origin resource sharing
+        mode: 'cors',
         headers: {
-          'Content-type': 'application/json',//Specify the Content-Type in the payload as JSON
-          'Authorization': `bearer ${token}`,// Attach the token in the Authorization header
+          'Content-type': 'application/json',//Specify the Content-Type 
+          'Authorization': `bearer ${token}`,// Attach the token 
         },
       });
 
-      // Conditional rendering if the response is OK (status code 200-299)
+      //Response handling
       if (!response.ok) {
-        throw new Error('Failed to remove user');// Throw an error if the DELETE request is unsuccessful
+        throw new Error('Failed to remove user');
       }
-      // Update the user list in the state to remove the deleted user
+      /* Update the user list in the state to remove the deleted user*/
       setUsers((prevUsers) =>
         prevUsers.filter((user) => user._id !== userId)
       );
-      console.log('User Successfully removed');// Log a success message in the console for debugging purposes
+      console.log('User Successfully removed');
 
     } catch (error) {
-      setError('Error removing user', error);// Set the error state to display the error in the UI
-      console.error('Error removing user:', error.message);//Log an error message in the console for debugging purposes
+      setError('Error removing user', error);
+      console.error('Error removing user:', error.message);
     }
   };
 
@@ -130,15 +134,16 @@ export default function Page4({//Export default Page4 function component
     const { name, value } = event.target;
     setEditUserData((prevData) => ({
       ...prevData,
-      [name]: value, // Update the property based on the input name
+      [name]: value,
     }));
   };
 
   // Function to toggle the update form
   const updateAccount = (userId) => {
-    // Toggle the current update form based on userId
-    // If the current userId matches the state, set it to null (hide form)
-    // Otherwise, set the state to the new userId (show form)
+    /*Toggle the current update form based on userId
+    If the current userId matches the state, set it to 
+    null (hide form) Otherwise, set the state to the 
+    new userId (show form)*/
     setUpdateUser(userId === updateUser ? null : userId);
   };
 
@@ -148,13 +153,14 @@ export default function Page4({//Export default Page4 function component
     /* Conditional rendering to check if the 
     current user is an admin user*/
     if (currentUser.admin) {
-      setIsUnauthorized(false);// Reset unauthorized state
-      setViewUsers(!viewUsers);// Toggle the visibility of the user list
+      setIsUnauthorized(false);
+      setViewUsers(!viewUsers);
     } 
     else {
-      console.log('Unauthorized');//Log a message in the console for debugging purposes
-      alert('Unauthorized');//Notify the user
-      setIsUnauthorized(true); // Set unauthorized state
+      console.log('Unauthorized');
+      alert('Unauthorized');
+      // Set unauthorized state
+      setIsUnauthorized(true); 
     }
   };
 
@@ -187,8 +193,7 @@ export default function Page4({//Export default Page4 function component
                 <div className='userData'>
                   <label>DATE OF BIRTH:</label>
                 <p className='outputText'>{dateDisplay(currentUser.dateOfBirth)}</p>
-                </div>
-                
+                </div>                
               </Col>
             </Row>
             <Row className='userOutputRow'>
