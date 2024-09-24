5584b1d911c2eedb2098fe8e1b42c9f17b0e0e38
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-09-24 14:17:09 +0200

    Update quizModel.js

--- task/server/models/quizModel.js
+++ task/server/models/quizModel.js
@@ -1,6 +1,6 @@
 // Import necessary modules and packages
 const mongoose = require('mongoose'); // Import the Mongoose library
-const autopopulate = require('mongoose-autopopulate');// Import the autopopulate plugin for Mongoose
+// const autopopulate = require('mongoose-autopopulate');// Import the autopopulate plugin for Mongoose
 
 // Define the schema for quizzes
 const quizSchema = new mongoose.Schema({
@@ -11,20 +11,20 @@ const quizSchema = new mongoose.Schema({
         unique: true,
         set: (v) => v.toUpperCase(),
     },
-    /*
     //Username of the person who created the quiz
     username: {
         type: String,//Define the datatype as a string
         required: [true, 'Username is required'],
-    },*/
+    },
+    /*
     //Username of the person who created the quiz
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'User',// Reference to the 'User' model
         // Automatically populate the quiz field
         autopopulate: true,
         required: [true, 'Username is required'], 
-    },/*
+    },
     userId: {
         type: mongoose.Schema.Types.ObjectId,// Define the datatype as ObjectId
         required: [true, 'Username is required'],            
@@ -82,5 +82,19 @@ quizSchema.index({userId: 1})
 // Apply the autopopulate plugin to the schema
 // quizSchema.plugin(autopopulate);
 
+// Middleware function that runs before a quiz is deleted
+/*quizSchema.pre('deleteOne', {document: true, query: false}, async function (next) {
+    try {
+        // Delete all associated scores for the quiz being deleted
+        // reference to the quiz name
+        await Score.deleteMany({ name: this.name });
+        console.log(`Deleted all scores for quiz: ${this.name}`);
+        next();// Proceed to the next middleware function
+        
+    } catch (error) {
+        console.error('Error deleting associated scores:', error);
+        next(error);// Proceed to the next middleware function
+    }
+})*/
 // Export the mongoose model for 'Quiz' using the defined schema
 module.exports = mongoose.model('Quiz', quizSchema);
