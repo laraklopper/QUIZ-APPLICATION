a4fa6707955f5784bce5a3d9b5239e12a76430b4
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-11-15 15:01:33 +0200

    Update scoreSchema.js

--- task/server/models/scoreSchema.js
+++ task/server/models/scoreSchema.js
@@ -1,63 +1,48 @@
 // Import necessary modules and packages
 const mongoose = require('mongoose');// Import the Mongoose library
-// const autopopulate = require('mongoose-autopopulate');// Import the autopopulate plugin for Mongoose
 
 //Define the score schema
 const scoreSchema = new mongoose.Schema({
-  /*   // Reference to the User who took the quiz
-      userId: {
-        type: mongoose.Schema.Types.ObjectId, // Define the data type as ObjectId
-        ref: 'User', // Reference the User model
-        required: [true, 'User ID is required'], // Ensure that userId is required with a custom message
-    },  
-      // Reference to the Quiz taken
-    quizId: {
-        type: mongoose.Schema.Types.ObjectId, // Define the data type as ObjectId
-        ref: 'Quiz', // Reference the Quiz model
-        required: [true, 'Quiz ID is required'], // Ensure that quizId is required with a custom message
-    },*/
-     // Field for the name of the quiz taken
-     name: {
-        type: String,
-        required:[true, 'Quiz name is required'],   
+    // Field for the name of the quiz taken
+    name: {
+        type: String,//Specify the data type as a string
+        required: [true, 'Quiz name is required'],//Mark the field as required
+        set: (v) => v.toUpperCase(),// Automatically convert to uppercase before saving
+        unique: false,//Specify the unique option as false
+        trim: true,// Remove leading and trailing whitespace
     },
-    /* Field for username of the user who
-    took the quiz*/
-    //  username: {
-    //     type: String,
-    //     required: [true, 'username is required'],
-    // },
+    //Field for username of the user who took the quiz
+     username: {
+        type: String,//Specify the data type as a 
+        required: [true, 'username is required'],
+         trim: true,// Remove leading and trailing whitespace
+    },
     //Field for the score
     score: {
         type: Number,// Define the type as Number
-        default: 0,
-        required: true,
-        //Ensure the score is an integer
-        set: (v) => Math.floor(v),
+        default: 0, // Set default value to 0
+        required: true,//Mark the field as required 
+        set: (v) => Math.floor(v),//Ensure the score is an integer
         /*Validate to ensure the score is a valid integer*/
         validate: {
-            validator: Number.isInteger,
-            message: 'Score must be an integer',//Custom error message if validation fails
+            validator: Number.isInteger,// Custom validator to enforce integer values
+            message: 'Score must be an integer',// Custom error message for validation failure
         },
     },
     //Field for the number of times the user has attempted the quiz
     attempts: {
-        type: Number,  
-        default: 1,
+        type: Number,  // Define the data type as Number
+        default: 1,// Set the default value to 1 for the first attempt
         set: (v) => Math.floor(v),
     },
     //Field for the date of the current attempt
     date: {
         type: Date,//Define the dataType as a date
-        default: Date.now,
+        default: Date.now,// Set the default to the current date and time
         required: true,
     },
-},{timestamps: true});
+},);
 
-// Apply autopopulate plugin to the schema
-// scoreSchema.plugin(autopopulate);
-
-// scoreSchema.index({ username: 1, name: 1 }, { unique: true });
 
 // Export the score model based on the scoreSchema
 module.exports = mongoose.model('Score', scoreSchema);
