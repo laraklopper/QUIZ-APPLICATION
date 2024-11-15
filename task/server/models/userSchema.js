eb4f72ccfe07a0b8960e541946377688f1931933
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-11-15 15:02:04 +0200

    Update userSchema.js

--- task/server/models/userSchema.js
+++ task/server/models/userSchema.js
@@ -1,26 +1,28 @@
 // Import necessary modules and packages
-const mongoose = require('mongoose');// Import the Mongoose library
+// Import the Mongoose library
+const mongoose = require('mongoose');
 
 // Define the user schema
 let userSchema = new mongoose.Schema({
     // Field for username
     username:{
         type: String,//Indicate data type as a string
-        required: [true, 'Username is required'],
+        required: [true, 'Username is required'],//Mark the username as required
         unique: true,// Ensure the username is unique
+        // set: (v) => v.toLowerCase(),
     },
     // Field for user email
     email:{
         type: String,//Indicate data type as a string
         required: [true, 'User email is required'],
-        unique: true,
+        unique: true,// Ensures no duplicate emails in the database
         // Automatically converts the email to lowercase before saving
-        set: (v) => toLowerCase(),
+        set: (v) => v.toLowerCase(),
     },
     //Field for user date of birth
     dateOfBirth: {
         type: Date,//Indicate data type as date
-        required: [true, 'date of birth is required'],
+        required: [true, 'date of birth is required'],//Mark the date of birth as required
     },
     //Optional field for admin status
     admin: {
@@ -29,9 +31,8 @@ let userSchema = new mongoose.Schema({
     },
     //Field for user password
     password: {
-        type: String, 
-        required: [true, 'User password is required'],
-        // unique: true, // Ensures the password is unique
+        type: String, //Indicate data type as a string
+        required: [true, 'User password is required'],//Mark the password as required
     }
 }, { timestamps: true });
 /* Add timestamps for each document, 
