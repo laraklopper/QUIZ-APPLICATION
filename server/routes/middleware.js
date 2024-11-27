19250d64a9b50ad8ed5b6f4614bdfce031717c14
laraklopper <135839853+laraklopper@users.noreply.github.com>
2024-11-27 14:33:43 +0200

    Update middleware.js

--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -1,6 +1,11 @@
 // Import necessary modules and packages
 const jwt = require('jsonwebtoken');// Import the 'jsonwebtoken' library for handling JSON Web Tokens
-const User = require('../models/userSchema')
+const User = require('../models/userSchema')//Import the user schema
+
+require('dotenv').config()
+// let secretKey = crypto.randomBytes(64).toString('hex');
+const secretKey = process.env.JWT_SECRET_KEY
+
 //Middleware function to check and verify a JWT token from the 'token' header
 const checkJwtToken = (req, res, next) => {
     const authHeader = req.headers.authorization// Retrieve the authorization header from the request
@@ -12,7 +17,7 @@ const checkJwtToken = (req, res, next) => {
         // Verify the token using the secret key
         const decoded = jwt.verify(
             token,
-            /*process.env.JWT_SECRET ||*/'secretKey',//Secret key used for signing the token stored in enviromental variables
+            process.env.JWT_SECRET ||'secretKey',//Secret key used for signing the token stored in enviromental variables
         );
         req.user = decoded;// Attach decoded user information to the request object
         console.log('Token provided');//Log a message in the console for debugging purposes
